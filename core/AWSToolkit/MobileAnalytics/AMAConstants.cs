3b672c98792733c1b9174e7c35474961949cb7ec
Tyler Moore <tymoore@amazon.com>
2015-07-13 15:15:42 -0700

    Added pre-compiler conditions for test vs prod accounts so you don't have to manually manage them.

--- core/AWSToolkit/MobileAnalytics/AMAConstants.cs
+++ core/AWSToolkit/MobileAnalytics/AMAConstants.cs
@@ -31,9 +31,19 @@ namespace Amazon.AWSToolkit.MobileAnalytics
         public static class ClientInformation
         {
             public const string CLIENT_ID = "anonymous_user";
-            public const string APP_TITLE = "visual-studio-toolkit-test";
-            public const string APP_ID = "9b7a8a27eec74c6696a15222961af9ae";
-        }
+
+            #if DEBUG
+            public const string APP_TITLE = "visual-studio-toolkit-test"; //test app
+            #else
+            public const string APP_TITLE = "visual-studio-toolkit-prod"; //prod app
+            #endif
+
+            #if DEBUG
+            public const string APP_ID = "9b7a8a27eec74c6696a15222961af9ae"; //test app
+            #else
+            public const string APP_ID = "ff4badf7ee1345b89e5b0fdc3a28be9e"; //prod app
+            #endif
+    }
 
         public static class EventTypes
         {
@@ -58,7 +68,10 @@ namespace Amazon.AWSToolkit.MobileAnalytics
         /// <summary>
         /// Cognito Identity Pool to use for the Mobile Analytics service.
         /// </summary>
-        public const string AWS_COGNITO_IDENTITY_POOL_ID = "us-east-1:93504934-38ab-4e75-aa41-d0f9bf5c5268";
-
+        #if DEBUG
+        public const string AWS_COGNITO_IDENTITY_POOL_ID = "us-east-1:93504934-38ab-4e75-aa41-d0f9bf5c5268"; //test app
+        #else
+        public const string AWS_COGNITO_IDENTITY_POOL_ID = "us-east-1:0ab25aed-7bb5-47df-9b30-df1aa3ec1bd3"; //prod app
+        #endif
     }
 }
