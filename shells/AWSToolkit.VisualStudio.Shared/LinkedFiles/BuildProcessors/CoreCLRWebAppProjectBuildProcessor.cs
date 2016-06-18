79d5e75314318d06fc5aa9a297d0106a4c6dac9e
Norm Johanson <normj@amazon.com>
2016-06-17 17:46:43 -0700

    Merge wizard settings into existing manifest file if it exists

--- shells/AWSToolkit.VisualStudio.Shared/LinkedFiles/BuildProcessors/CoreCLRWebAppProjectBuildProcessor.cs
+++ shells/AWSToolkit.VisualStudio.Shared/LinkedFiles/BuildProcessors/CoreCLRWebAppProjectBuildProcessor.cs
@@ -9,6 +9,8 @@ using Microsoft.VisualStudio.Shell.Interop;
 
 using ICSharpCode.SharpZipLib.Zip;
 
+using ThirdParty.Json.LitJson;
+
 using Microsoft.Build.Framework;
 using MSBuildProject = Microsoft.Build.Evaluation.Project;
 using EnvDTEProject = EnvDTE.Project;
@@ -214,13 +216,62 @@ namespace Amazon.AWSToolkit.VisualStudio.Shared.BuildProcessors
                 iisAppPath = iisPath.Substring(pos);
             }
 
-            var manifest = DEFAULT_MANIFEST.Replace("{iisPath}", iisAppPath).Replace("{iisWebSite}", iisWebSite);
+            var pathToManifest = Path.Combine(publishLocation, "aws-windows-deployment-manifest.json");
+            string manifest;
+            if (File.Exists(pathToManifest))
+            {
+                Func<string, JsonData, JsonData> getOrCreateNode = (name, node) =>
+                {
+                    JsonData child = node[name] as JsonData;
+                    if(child == null)
+                    {
+                        child = new JsonData();
+                        node[name] = child;
+                    }
+                    return child;
+                };
 
-            var path = Path.Combine(publishLocation, "aws-windows-deployment-manifest.json");
-            if (File.Exists(path))
-                File.Delete(path);
+                JsonData root = JsonMapper.ToObject(File.ReadAllText(pathToManifest));
+                if (root["manifestVersion"] == null || !root["manifestVersion"].IsInt)
+                {
+                    root["manifestVersion"] = 1;
+                }
 
-            File.WriteAllText(path, manifest);
+                JsonData deploymentNode = getOrCreateNode("deployments", root);
+
+                JsonData aspNetCoreWebNode = getOrCreateNode("aspNetCoreWeb", deploymentNode);
+
+                JsonData appNode;
+                if (aspNetCoreWebNode.GetJsonType() == JsonType.None || aspNetCoreWebNode.Count == 0)
+                {
+                    appNode = new JsonData();
+                    aspNetCoreWebNode.Add(appNode);
+                }
+                else 
+                    appNode = aspNetCoreWebNode[0];
+
+
+                if (appNode["name"] == null || !appNode["name"].IsString || string.IsNullOrEmpty((string)appNode["name"]))
+                {
+                    appNode["name"] = "app";
+                }
+
+                JsonData parametersNode = getOrCreateNode("parameters", appNode);
+                parametersNode["appBundle"] = ".";
+                parametersNode["iisPath"] = iisAppPath;
+                parametersNode["iisWebSite"] = iisWebSite;
+
+                manifest = root.ToJson();
+            }
+            else
+            {
+                manifest = DEFAULT_MANIFEST.Replace("{iisPath}", iisAppPath).Replace("{iisWebSite}", iisWebSite);
+
+                if (File.Exists(pathToManifest))
+                    File.Delete(pathToManifest);
+            }
+
+            File.WriteAllText(pathToManifest, manifest);
         }
     }
 }
