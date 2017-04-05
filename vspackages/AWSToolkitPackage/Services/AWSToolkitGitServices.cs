a606c82067824e13274faaf710cb591bef28d174
Steve Roberts <strobe@amazon.com>
2017-04-05 12:59:43 -0700

    Cleaned up team foundation references to bjild in vs2015

--- vspackages/AWSToolkitPackage/Services/AWSToolkitGitServices.cs
+++ vspackages/AWSToolkitPackage/Services/AWSToolkitGitServices.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using Amazon.AWSToolkit.Account;
 using Amazon.AWSToolkit.Shared;
 using log4net;
 using Microsoft.VisualStudio.Shell.Interop;
+
+#if VS2017_OR_LATER
 using Microsoft.VisualStudio.TeamFoundation.Git.Extensibility;
+#endif
+
+#if VS2015_OR_LATER
+using Microsoft.TeamFoundation.Git.Controls.Extensibility;
+#endif
 
 namespace Amazon.AWSToolkit.VisualStudio.Services
 {
@@ -36,13 +44,16 @@ namespace Amazon.AWSToolkit.VisualStudio.Services
                     progress.ProgressChanged += (s, e) => _statusBar.SetText(e.ProgressText);
                     await gitExt.CloneAsync(repositoryUrl, destinationFolder, recurseSubmodules, default(CancellationToken), progress);
                 });
-#else
-            var gitExt = HostPackage.GetVSShellService(typeof(IGitRepositoriesExt)) as IGitRepositoriesExt;
-            gitExt.Clone(repositoryUrl, destinationFolder, recurseSubmodules ? CloneOptions.RecurseSubmodule : CloneOptions.None);
+#elif VS2015
+                var gitExt = HostPackage.GetVSShellService(typeof(IGitRepositoriesExt)) as IGitRepositoriesExt;
+                gitExt.Clone(repositoryUrl, destinationFolder, recurseSubmodules ? CloneOptions.RecurseSubmodule : CloneOptions.None);
 
-            // The operation will have completed when CanClone goes false and then true again.
-            await gitExt.WhenAnyValue(x => x.CanClone).Where(x => !x).Take(1);
-            await gitExt.WhenAnyValue(x => x.CanClone).Where(x => x).Take(1);
+                // todo: disabled, as WhenAnyValue is a reactive lib extension -- need to figure alternative or use the lib
+                /*
+                // The operation will have completed when CanClone goes false and then true again.
+                await gitExt.WhenAnyValue(x => x.CanClone).Where(x => !x).Take(1);
+                await gitExt.WhenAnyValue(x => x.CanClone).Where(x => x).Take(1);
+                */
 #endif
             }
             catch (Exception e)
