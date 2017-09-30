7781495f0fafac54d4113eb4c94159e7aaee4e28
Norm Johanson <normj@amazon.com>
2017-09-30 00:29:01 -0700

    Setup ECS plugin projects

---
+++ toolkitcore/plugins/AWSToolkit.ECS/ECSActivator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon.AWSToolkit.ECS
+{
+    public class ECSActivator : AbstractPluginActivator
+    {
+        public override string PluginName => "ECS";
+
+        public override void RegisterMetaNodes()
+        {
+
+        }
+    }
+}
---
+++ toolkitcore/plugins/AWSToolkit.ECS/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AWSToolkit.ECS")]
+[assembly: AssemblyDescription("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a09c427c-dbf7-49c8-9590-e99ad60dece2")]
+
+
