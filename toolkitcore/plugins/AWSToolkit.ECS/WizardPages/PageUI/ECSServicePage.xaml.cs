7207db9a333917f71fd2ce8dc814051dc5bd237c
Norm Johanson <normj@amazon.com>
2017-11-08 13:05:49 -0800

    Match the console for default values for min and max health

--- toolkitcore/plugins/AWSToolkit.ECS/WizardPages/PageUI/ECSServicePage.xaml.cs
+++ toolkitcore/plugins/AWSToolkit.ECS/WizardPages/PageUI/ECSServicePage.xaml.cs
@@ -98,8 +98,8 @@ namespace Amazon.AWSToolkit.ECS.WizardPages.PageUI
                     this._ctlDesiredCount.Text = "1";
                 }
 
-                this._ctlMinimumHealthy.Text = "50";
-                this._ctlMaximumPercent.Text = "100";
+                this._ctlMinimumHealthy.Text = "100";
+                this._ctlMaximumPercent.Text = "200";
 
                 if (!this._ctlServicePicker.Items.Contains(PageController.HostingWizard[PublishContainerToAWSWizardProperties.SafeProjectName] as string))
                     this._ctlNewServiceName.Text = PageController.HostingWizard[PublishContainerToAWSWizardProperties.SafeProjectName] as string;
