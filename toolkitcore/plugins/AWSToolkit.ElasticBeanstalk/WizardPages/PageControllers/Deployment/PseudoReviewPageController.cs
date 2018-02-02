1acc67a7737e9d98c95dba756344fc670dc69690
Norm Johanson <normj@amazon.com>
2018-02-02 09:40:26 -0800

    Add AWS X-Ray to beanstalk wizard review page

--- toolkitcore/plugins/AWSToolkit.ElasticBeanstalk/WizardPages/PageControllers/Deployment/PseudoReviewPageController.cs
+++ toolkitcore/plugins/AWSToolkit.ElasticBeanstalk/WizardPages/PageControllers/Deployment/PseudoReviewPageController.cs
@@ -361,6 +361,15 @@ namespace Amazon.AWSToolkit.ElasticBeanstalk.WizardPages.PageControllers.Deploym
                     sb.AppendLine(string.Format("Use '{0}' as the URL for health-checks.", healthUrl));
             }
 
+            if(HostingWizard.IsPropertySet(BeanstalkDeploymentWizardProperties.ApplicationProperties.propkey_EnableXRayDaemon))
+            {
+                var enableXRay = (bool)HostingWizard[BeanstalkDeploymentWizardProperties.ApplicationProperties.propkey_EnableXRayDaemon];
+                if(enableXRay)
+                {
+                    sb.Append("Enable AWS X-Ray Daemon.");
+                }
+            }
+
             if (!isCoreCLRDeployment)
             {
                 IDictionary<string, string> appSettings = null;
