6ce85cac51cbbd9434ce5a80cb4e3e78f5caf24b
Norm Johanson <normj@amazon.com>
2018-06-06 11:05:46 -0700

    Fixed issue with showing the incorrect ECR Repository URI

--- toolkitcore/plugins/AWSToolkit.ECS/Model/ViewRepositoryModel.cs
+++ toolkitcore/plugins/AWSToolkit.ECS/Model/ViewRepositoryModel.cs
@@ -60,7 +60,7 @@ namespace Amazon.AWSToolkit.ECS.Model
 
         public string DockerPushCommand
         {
-            get { return string.Format("docker push {0}/{1}:latest", _repositoryWrapper.RepositoryUri, _repositoryWrapper.Name); }
+            get { return string.Format("docker push {0}:latest", _repositoryWrapper.RepositoryUri); }
         }
         #endregion
 
