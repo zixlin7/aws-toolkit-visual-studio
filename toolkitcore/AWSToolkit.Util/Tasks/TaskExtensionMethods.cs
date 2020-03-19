23eb7a3fd74d6c27f811d304dfdd770fa4a123ea
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2020-03-19 09:48:02 -0700

    Improve Toolkit Startup time by initializing Metrics in Tasks
    Starting Visual Studio is directly impacted by Toolkit startup time. Visual Studio does not display until the Toolkit has completed starting up.
    
    This change improves toolkit startup time by pushing some of the telemetry activation into tasks.
    
    A telemetry client is instantiated as part of the Toolkit's setup. Part of the instantiation involves getting Cognito credentials, which adds to the toolkit startup time. Obtaining credentials has been moved to a background task, and they are provided to the telemetry system once they are available. The telemetry system will queue incoming metrics as normal, and will not send metrics until the deferred initialization is complete.
    
    On my system, on average, the telemetry initialization went from 0.7 seconds to 0.3 seconds. On the surface this may not seem very consequential. Previously, initialization time in this space correlated to network connections and latency. Now, initialization time is more constant. This change will be a more remarkable improvement when run on less performant network connections (eg: wifi) and from more remote locations.
    
    Additional changes:
    
    * new extension method to cap off fire and forget tasks, so that we log any exceptions that arise. We may do more fire and forget work in the future
    * The CloudFormation Stack Resource Pane received non-related changes due to warnings that arose during development

---
+++ toolkitcore/AWSToolkit.Util/Tasks/TaskExtensionMethods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using log4net;
+
+namespace Amazon.AWSToolkit.Tasks
+{
+    public static class TaskExtensionMethods
+    {
+        internal static ILog LOGGER = LogManager.GetLogger(typeof(TaskExtensionMethods));
+
+        /// <summary>
+        /// Consumes a Task, ignores the result, and logs any Exceptions that arose in the task.
+        /// Handy for calls that do not care about a Task's completion.
+        /// Prevents unobserved Exceptions when firing and forgetting Tasks.
+        /// </summary>
+        /// <see cref="https://github.com/microsoft/vs-threading/blob/master/doc/cookbook_vs.md#how-to-write-a-fire-and-forget-method-responsibly"/>
+        /// <param name="task">The task to consume.</param>
+        public static void LogExceptionAndForget(this Task task)
+        {
+            if (!task.IsCompleted || task.IsFaulted || task.IsCanceled)
+            {
+                // Silence "missing await" warning with Discard operator
+                _ = AwaitAndLogException(task);
+            }
+        }
+
+        private static async Task AwaitAndLogException(Task task)
+        {
+            try
+            {
+                await task;
+            }
+            catch (Exception e)
+            {
+                LOGGER.Error($"Encountered Exception in Task", e);
+            }
+        }
+    }
+}
