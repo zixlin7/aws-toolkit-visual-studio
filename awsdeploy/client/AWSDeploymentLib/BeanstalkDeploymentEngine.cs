zixlin7/aws-toolkit-visual-studio

commit 541d58823332bd943a4ab23e36cf296da4270e57

Fix for deploying to Beanstalk and getting a RED health check (#45)
When the toolkit creates a service role during a Beanstalk deployment, the role does not have elasticloadbalancing:DescribeTargetHealth permissions. This causes multi instance ALB health to get reported as RED.

This change generates a role that uses the managed policy arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkService. The following permissions are no longer in the role as a result:

- ec2:DescribeInstanceStatus
- ec2:GetConsoleOutput

Out of scope: Some sort of detection and/or notification for users that already have a role, which does not contain the elasticloadbalancing:DescribeTargetHealth permission.

Tested by...

- deleting all aws-elasticbeanstalk-* roles from my account
- deploying an asp.net core app to beanstalk, with a multi instance ALB
- app deployed, successfully showed a GREEN health status

awsdeploy/client/AWSDeploymentLib/BeanstalkDeploymentEngine.cs
@@ -1484,10 +1484,9 @@ namespace AWSDeployment
                         };
                         role = IAMClient.CreateRole(request).Role;
 
-                        IAMClient.PutRolePolicy(new PutRolePolicyRequest
+                        IAMClient.AttachRolePolicy(new AttachRolePolicyRequest
                         {
-                            PolicyDocument = RolePolicies.DefaultBeanstalkServiceRolePolicy,
-                            PolicyName = serviceRoleName + "_VSToolkit-autocreated",
+                            PolicyArn = RolePolicies.ServiceRoleArns.AWSElasticBeanstalkService,
                             RoleName = serviceRoleName
                         });
                     }

awsdeploy/client/AWSDeploymentLib/DeploymentConstants.cs
@@ -91,32 +91,9 @@
 
     public abstract class RolePolicies
     {
-        public const string DefaultBeanstalkServiceRolePolicy = 
-"{"
-  + "\"Version\": \"2012-10-17\","
-  + "\"Statement\": ["
-  + " {"
-  + "   \"Effect\": \"Allow\","
-  + "    \"Action\": ["
-  + "      \"elasticloadbalancing:DescribeInstanceHealth\","
-  + "      \"ec2:DescribeInstances\","
-  + "      \"ec2:DescribeInstanceStatus\","
-  + "      \"ec2:GetConsoleOutput\","
-  + "      \"ec2:AssociateAddress\","
-  + "      \"ec2:DescribeAddresses\","
-  + "      \"ec2:DescribeSecurityGroups\","
-  + "      \"sqs:GetQueueAttributes\","
-  + "      \"sqs:GetQueueUrl\","
-  + "      \"autoscaling:DescribeAutoScalingGroups\","
-  + "      \"autoscaling:DescribeAutoScalingInstances\","
-  + "      \"autoscaling:DescribeScalingActivities\","
-  + "      \"autoscaling:DescribeNotificationConfigurations\""
-  + "    ],"
-  + "    \"Resource\": ["
-  + "      \"*\""
-  + "    ]"
-  + "  }"
-  + "]"
-  + "}";
+        public static class ServiceRoleArns
+        {
+            public const string AWSElasticBeanstalkService = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkService";
+        }
     }
 }

commit aca94b14af7565c2b111138a721c025eb49d83e5

Fix regression where single-instance Beanstalk deploys would report failed health checks
This change fixes a bug introduced in 541d588 where single instance deployments to Beanstalk would fail to report health checks. AWSElasticBeanstalkEnhancedHealth is now attached to toolkit generated beanstalk service roles.

Tested by deploying the following combinations to Beanstalk, verifying the web application was accessible, and that the Beanstalk environment has a Green health check:

* asp.NET Framework app: single instance, multi instance Application Load Balancer, multi instance Classic Load Balancer
* asp.NET Core app: single instance, multi instance Application Load Balancer, multi instance Classic Load Balancer

awsdeploy/client/AWSDeploymentLib/BeanstalkDeploymentEngine.cs
@@ -1489,6 +1489,12 @@ namespace AWSDeployment
                             PolicyArn = RolePolicies.ServiceRoleArns.AWSElasticBeanstalkService,
                             RoleName = serviceRoleName
                         });
+
+                        IAMClient.AttachRolePolicy(new AttachRolePolicyRequest
+                        {
+                            PolicyArn = RolePolicies.ServiceRoleArns.AWSElasticBeanstalkEnhancedHealth,
+                            RoleName = serviceRoleName
+                        });
                     }
                     else
                     {

awsdeploy/client/AWSDeploymentLib/DeploymentConstants.cs
@@ -93,7 +93,11 @@
     {
         public static class ServiceRoleArns
         {
-            public const string AWSElasticBeanstalkService = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkService";
+            public const string AWSElasticBeanstalkService =
+                "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkService";
+
+            public const string AWSElasticBeanstalkEnhancedHealth =
+                "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkEnhancedHealth";
         }
     }
 }