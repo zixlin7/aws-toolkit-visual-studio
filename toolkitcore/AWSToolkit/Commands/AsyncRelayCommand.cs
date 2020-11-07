88416cfd8ea0df95d23ac9e2936d446096faacfe
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2020-11-06 16:57:00 -0800

    RelayCommand (#116)
    This change adds some Utility methods that make it easier to use WPF DataBinding with functions (via ICommand). These will be used in upcoming changes.
    
    The benefit of using this pattern is that the RelayCommands helps manage the enabled state when bound to UI elements.

---
+++ toolkitcore/AWSToolkit/Commands/AsyncRelayCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Amazon.AWSToolkit.Tasks;
+
+namespace Amazon.AWSToolkit.Commands
+{
+    public interface IAsyncCommand : ICommand
+    {
+        Task ExecuteAsync(object parameter);
+    }
+
+    /// <summary>
+    /// Utility class allowing for easy WPF DataBinding to async-based functionality.
+    /// </summary>
+    public class AsyncRelayCommand : IAsyncCommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private readonly Func<object, bool> _canExecute;
+        private readonly Func<object, Task> _execute;
+
+        public AsyncRelayCommand(Func<object, Task> execute)
+            : this(null, execute)
+        {
+        }
+
+        public AsyncRelayCommand(Func<object, bool> canExecute, Func<object, Task> execute)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public async Task ExecuteAsync(object parameter)
+        {
+            if (CanExecute(parameter))
+            {
+                await _execute(parameter);
+            }
+
+            RaiseCanExecuteChanged();
+        }
+
+        public void Execute(object parameter)
+        {
+            ExecuteAsync(parameter).LogExceptionAndForget();
+        }
+
+        private void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ toolkitcore/AWSToolkit/Commands/RelayCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Amazon.AWSToolkit.Commands
+{
+    /// <summary>
+    /// Utility class allowing for easy WPF DataBinding to functionality.
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private readonly Func<object, bool> _canExecute;
+        private readonly Action<object> _execute;
+
+        public RelayCommand(Action<object> execute)
+            : this(null, execute)
+        {
+        }
+
+        public RelayCommand(Func<object, bool> canExecute, Action<object> execute)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (CanExecute(parameter))
+            {
+                _execute?.Invoke(parameter);
+            }
+
+            RaiseCanExecuteChanged();
+        }
+
+        private void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
