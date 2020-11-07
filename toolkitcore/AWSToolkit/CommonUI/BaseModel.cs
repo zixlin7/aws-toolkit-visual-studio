1d4c76be4c78258cd25d0d964ddf72d8ee9dd922
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2020-11-06 16:56:28 -0800

    SetProperty (#117)
    This change adds a utility method to set a property and raise a property-changed notification, but only if the property value differs. This will be used in some upcoming changes.

--- toolkitcore/AWSToolkit/CommonUI/BaseModel.cs
+++ toolkitcore/AWSToolkit/CommonUI/BaseModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq.Expressions;
 
 namespace Amazon.AWSToolkit.CommonUI
 {
@@ -11,6 +12,25 @@ namespace Amazon.AWSToolkit.CommonUI
         {
             if (PropertyChanged != null)
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Sets the property value if different, and notifies of a change
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="propertyField">Field that stores the property value</param>
+        /// <param name="value">Value to assign to the property</param>
+        /// <param name="property">Expression that resolves to the Property being set (this is used to derive the property name)</param>
+        protected void SetProperty<T>(ref T propertyField, T value, Expression<Func<T>> property)
+        {
+            if (object.Equals(propertyField, value)) return;
+
+            propertyField = value;
+
+            if (property.Body is MemberExpression memberExpression)
+            {
+                NotifyPropertyChanged(memberExpression.Member.Name);
+            }
         }
     }
 }
