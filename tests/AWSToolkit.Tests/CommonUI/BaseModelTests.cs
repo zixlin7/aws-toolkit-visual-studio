00228317c81924d5512f6c41e5d1fb57a2ffcf07
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2021-05-24 17:39:06 -0700

    SetProperty with automatic field name (#370)
    This change adds a new SetProperty method that automatically detects the property name when raising PropertyChanged events. This removes the need to pass the property of interest in when calling SetProperty.
    
    This change also brings BaseModel.SetProperty under test.

---
+++ tests/AWSToolkit.Tests/CommonUI/BaseModelTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using Amazon.AWSToolkit.CommonUI;
+
+using Xunit;
+
+namespace AWSToolkit.Tests.CommonUI
+{
+    public class BaseModelTests
+    {
+        public class TestModel : BaseModel
+        {
+            private string _value;
+
+            public string ValueUsingImpliedSetter
+            {
+                get => _value;
+                set => SetProperty(ref _value, value);
+            }
+
+            public string ValueUsingMemberExpression
+            {
+                get => _value;
+                set => SetProperty(ref _value, value, () => ValueUsingMemberExpression);
+            }
+        }
+
+        private readonly TestModel _sut = new TestModel();
+        private readonly string _sampleValue = Guid.NewGuid().ToString();
+
+        [Fact]
+        public void SetProperty_ImpliedPropertyName()
+        {
+            Assert.PropertyChanged(_sut,
+                nameof(TestModel.ValueUsingImpliedSetter),
+                () => _sut.ValueUsingImpliedSetter = _sampleValue);
+
+            Assert.Equal(_sampleValue, _sut.ValueUsingImpliedSetter);
+        }
+
+        [Fact]
+        public void SetProperty_MemberExpression()
+        {
+            Assert.PropertyChanged(_sut,
+                nameof(TestModel.ValueUsingMemberExpression),
+                () => _sut.ValueUsingMemberExpression = _sampleValue);
+
+            Assert.Equal(_sampleValue, _sut.ValueUsingMemberExpression);
+        }
+    }
+}
