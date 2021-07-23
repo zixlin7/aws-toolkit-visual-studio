0567704fafc401d4be423e0b6fd1ea67be4e14be
JadenSimon <31319484+JadenSimon@users.noreply.github.com>
2021-07-23 09:48:23 -0700

    Add release script (#460)
    * Adds script for automating creation of release candidates

---
+++ buildtools/release/README.md
@@ -0,0 +1,11 @@
+### Setup
+- Install Python 3.9+
+  - Script was tested with 3.9.6
+
+### Usage
+- Run the following with Developer PowerShell for VS 2019 in the root of the repository
+```
+msbuild buildtools\build.proj /t:queueCandidate
+```
+
+
---
+++ buildtools/release/release.ps1
@@ -0,0 +1,19 @@
+param (
+    [Parameter(Mandatory=$true)][string]$repoRoot
+)
+
+# vshwere can be found here for VS 15.2+
+$VSWHERE = "${env:ProgramFiles(X86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+
+# Locates a Visual Studio install of at least version 16.0
+$VSINSTALLDIR = & $VSWHERE -version 16.0 -property installationPath
+
+# Developer shell entry point
+$DEVSHELL = "${VSINSTALLDIR}\Common7\Tools\Launch-VsDevShell.ps1"
+& $DEVSHELL
+
+# Produces changelog notes before the release script pushes the candidate branch
+$GENERATE_NOTES = "msbuild buildtools\changelog.proj /t:createRelease /p:ReleaseVersion=`%RELEASE_VERSION`%"
+
+cd $repoRoot
+python buildtools\release\release.py --hooks $GENERATE_NOTES
