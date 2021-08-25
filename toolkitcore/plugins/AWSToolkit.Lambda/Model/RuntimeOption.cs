2940535eebd6fc3a1d915a0c7783ea9c0d0ad52d
Shruti Sinha <44882001+shruti0085@users.noreply.github.com>
2021-08-25 12:24:30 -0700

    Remove NodeJS10 from Lambda Upload dialogs (#567)

--- toolkitcore/plugins/AWSToolkit.Lambda/Model/RuntimeOption.cs
+++ toolkitcore/plugins/AWSToolkit.Lambda/Model/RuntimeOption.cs
@@ -4,7 +4,6 @@ namespace Amazon.AWSToolkit.Lambda.Model
 {
     public class RuntimeOption
     {
-        public static readonly RuntimeOption NodeJS_v10_X = new RuntimeOption(Amazon.Lambda.Runtime.Nodejs10X.Value, "Node.js v10");
         public static readonly RuntimeOption NodeJS_v12_X = new RuntimeOption(Amazon.Lambda.Runtime.Nodejs12X.Value, "Node.js v12");
 
         public static readonly RuntimeOption NetCore_v2_1 = new RuntimeOption(Amazon.Lambda.Runtime.Dotnetcore21.Value, ".NET Core v2.1");
@@ -12,7 +11,7 @@ namespace Amazon.AWSToolkit.Lambda.Model
 
         public static readonly RuntimeOption PROVIDED = new RuntimeOption(Amazon.Lambda.Runtime.Provided.Value, "Custom .NET Core Runtime");
 
-        public static readonly RuntimeOption[] ALL_OPTIONS = new RuntimeOption[] { NetCore_v2_1, NetCore_v3_1, NodeJS_v10_X, NodeJS_v12_X, PROVIDED };
+        public static readonly RuntimeOption[] ALL_OPTIONS = new RuntimeOption[] { NetCore_v2_1, NetCore_v3_1, NodeJS_v12_X, PROVIDED };
 
         private RuntimeOption(string value, string displayName)
         {
