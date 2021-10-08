13818bd842e1a6595dd5c5699d86988c3565755d
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2021-10-08 11:50:59 -0700

    Generate test builds (#700)
    This change makes it easier to generate (or obtain) VSIX builds that can be used for local or internal testing.
    
    Previously, I would look up the current Toolkit version, come up with a semi-incremented value from that, try to remember the msbuild incantation to run the `update-version` task, then compile with the Release configuration. This is now all automated with the build task `build-test-toolkit`.
    
    The intention is to run `build-test-toolkit` locally from a commit or branch of interest, whenever someone needs to produce and share a copy of the Toolkit for testing. This has also been hooked into GitHub Actions so that we can grab a compiled VSIX directly from PRs and commits.
    
    These test builds are given a version that starts with the most recently released Toolkit version, then adds in the current month+day. This gives us Good Enough granularity to access test builds on a daily basis without having to worry about uninstalling an existing build before installing these auto-generated builds.
    
    When the `build-test-toolkit` task is used locally, you can also provide an explicit version by using the optional `Version` parameter: `msbuild buildtools\build.proj /t:build-test-toolkit /p:Version=1.2.3.4`

---
+++ buildtasks/BuildTasks/VersionManagement/CreateTestVersionTask.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Microsoft.Build.Framework;
+
+namespace BuildTasks.VersionManagement
+{
+    /// <summary>
+    /// Produces a version suitable for making test builds on a daily basis.
+    /// The 4th version number (Revision) is altered by applying a date-stamp to it.
+    /// Example: A version of 1.22.333.4 On Oct 7 is transformed to 1.22.333.10074
+    /// </summary>
+    public class CreateTestVersionTask : BuildTaskBase
+    {
+        public string InitialVersion { get; set; }
+
+        [Output]
+        public string Version { get; set; }
+
+        public override bool Execute()
+        {
+            return Execute(DateTime.Now);
+        }
+
+        /// <summary>
+        /// Overload allows for easier testing with a specific time
+        /// </summary>
+        /// <param name="time">Date used in producing the version number</param>
+        public bool Execute(DateTime time)
+        {
+            var version = System.Version.Parse(InitialVersion);
+
+            Version = new Version(
+                version.Major,
+                version.Minor,
+                version.Build,
+                GenerateTestRevision(time, version.Revision)).ToString();
+
+            return true;
+        }
+
+        private static int GenerateTestRevision(DateTime dateTime, int currentRevision)
+        {
+            // Version values cannot exceed 65534 (when building VSIX files)
+            // The maximum value (12310) added to the revision remains below this limit.
+            var month = dateTime.Month;
+            var day = dateTime.Day;
+            return (month * 1000) + (day * 10) + currentRevision;
+        }
+    }
+}
---
+++ buildtasks/BuildTasks/VersionManagement/GetVersionTask.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+
+using Microsoft.Build.Framework;
+
+using Newtonsoft.Json;
+
+namespace BuildTasks.VersionManagement
+{
+    /// <summary>
+    /// Retrieves the value of a given JSON file's "version" field
+    /// </summary>
+    public class GetVersionTask : BuildTaskBase
+    {
+        internal class ReleaseConfigManifest
+        {
+            public string Version { get; set; }
+        }
+
+        /// <summary>
+        /// JSON file to read the "version" field from
+        /// </summary>
+        public string Filename { get; set; }
+
+        /// <summary>
+        /// Value of version field loaded from <see cref="Filename"/>
+        /// </summary>
+        [Output]
+        public string Version { get; set; }
+
+        public override bool Execute()
+        {
+            var json = File.ReadAllText(Filename);
+            var manifest = JsonConvert.DeserializeObject<ReleaseConfigManifest>(json);
+
+            Version = manifest.Version;
+            return true;
+        }
+    }
+}
