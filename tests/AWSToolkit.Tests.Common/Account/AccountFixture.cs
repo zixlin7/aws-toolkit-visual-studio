fd72a603f7658ded8092c5c1af5571ea2500dea4
Kody Fintak <15849743+KodyFintak@users.noreply.github.com>
2021-11-10 10:58:08 -0800

    Extracting factory methods of Accounts in AccountViewModelGroupConverterTests to AccountFixture. (#786)

--- tests/AWSToolkit.Tests.Common/Account/AccountFixture.cs
+++ tests/AWSToolkit.Tests.Common/Account/AccountFixture.cs
@@ -1,5 +1,6 @@
 ﻿using Amazon.AWSToolkit.Account;
 using Amazon.AWSToolkit.Credentials.Core;
+using Amazon.AWSToolkit.Tests.Common.Context;
 
 namespace Amazon.AWSToolkit.Tests.Common.Account
 {
@@ -7,7 +8,22 @@ namespace Amazon.AWSToolkit.Tests.Common.Account
     {
         public static AccountViewModel CreateSharedCredentialAccount()
         {
-            return new AccountViewModel(null, null, new SharedCredentialIdentifier("sharedProfile"), null);
+            return CreateAccountWith(new SharedCredentialIdentifier("sharedProfile"));
+        }
+
+        private static AccountViewModel CreateAccountWith(ICredentialIdentifier credentialIdentifier)
+        {
+            return new AccountViewModel(null, null, credentialIdentifier, null);
+        }
+
+        public static AccountViewModel CreateSdkCredentialAccount()
+        {
+            return CreateAccountWith(new SDKCredentialIdentifier("sdkProfile"));
+        }
+
+        public static AccountViewModel CreateFakeCredentialAccount()
+        {
+            return CreateAccountWith(FakeCredentialIdentifier.Create("altProfile"));
         }
     }
 }
