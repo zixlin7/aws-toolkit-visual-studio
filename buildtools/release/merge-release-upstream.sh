f8d9385e6c22b938d441e3b804225f254b0a245b
Kody Fintak <15849743+KodyFintak@users.noreply.github.com>
2021-12-06 10:30:15 -0800

    Modify body for failed to push automatic prs (#831)

--- buildtools/release/merge-release-upstream.sh
+++ buildtools/release/merge-release-upstream.sh
@@ -17,13 +17,20 @@ echo "[Release] Pipeline is in production. Pushing commit to GitHub."
 create_merge_pr() {
     git checkout -b $1
     git push --follow-tags -u origin $1
-    gh pr create --base $DEVELOPMENT_BRANCH --title "Merge $RELEASE_BRANCH changes into $DEVELOPMENT_BRANCH (${NEXT_VERSION})" --body "$MERGE_PR_BODY"
+    gh pr create --base $DEVELOPMENT_BRANCH --title "Merge $RELEASE_BRANCH changes into $DEVELOPMENT_BRANCH (${NEXT_VERSION})" --body "$2"
 }
 
 if ! [ -z $CONFLICT ]; then
     echo "[Release] Merge conlict detected. Opening a PR."
+
     BRANCH_NAME="release/merge-conflict/${NEXT_VERSION}"
-    create_merge_pr $BRANCH_NAME
+
+    PR_BODY="Automatic merge failed due to conflicts.
+Manual resolution is required.
+# Merges _must_ be done by merge commit."
+
+    create_merge_pr $BRANCH_NAME "$PR_BODY"
+
     exit 0;
 fi
 
@@ -31,6 +38,12 @@ git push origin
 
 if [ $? -ne 0 ]; then
     echo "[Release] Failed to push to branch. Opening a PR."
+
     BRANCH_NAME="release/manual-merge/${NEXT_VERSION}"
-    create_merge_pr $BRANCH_NAME
+
+    PR_BODY="Automatic merge failed to push directly.
+Manual resolution is required.
+# Merge _must_ be done by merge commit."
+
+    create_merge_pr $BRANCH_NAME "$PR_BODY"
 fi
