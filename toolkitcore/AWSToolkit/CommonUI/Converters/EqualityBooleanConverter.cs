0070a4755ce6c06ce6572d7214bca9d8eb5f9bc6
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2021-12-10 10:08:13 -0800

    EqualityBooleanConverter (#840)
    This change introduces a ValueConverter class that returns true if the bound value matches the "expected" value that is provided as a parameter. My use case is for an upcoming change where I bind which radio button is checked based on an enum value.

---
+++ toolkitcore/AWSToolkit/CommonUI/Converters/EqualityBooleanConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Amazon.AWSToolkit.CommonUI.Converters
+{
+    /// <summary>
+    /// Returns true if the given value is equal to the given parameter.
+    /// Handy for Enum comparisons.
+    /// </summary>
+    public class EqualityBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.Equals(parameter) ?? parameter == null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool valueBool && valueBool)
+            {
+                return parameter;
+            }
+
+            return Binding.DoNothing;
+        }
+    }
+}
