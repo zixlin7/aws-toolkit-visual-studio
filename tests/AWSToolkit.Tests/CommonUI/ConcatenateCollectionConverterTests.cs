17a1f2b9d4ae1688556724c07a10f5019e71f6d7
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2021-12-13 16:48:07 -0800

    ConcatenateCollectionConverter (#844)
    This change introduces a value converter, used to display join lists of objects (strings) with a separator when they are shown in a UI. It will be used by an upcoming change.

---
+++ tests/AWSToolkit.Tests/CommonUI/ConcatenateCollectionConverterTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using Amazon.AWSToolkit.CommonUI.Converters;
+
+using Xunit;
+
+namespace AWSToolkit.Tests.CommonUI
+{
+    public class ConcatenateCollectionConverterTests
+    {
+        private readonly ConcatenateCollectionConverter _sut = new ConcatenateCollectionConverter();
+        private static readonly IList<string> SampleTextCollection = new List<string>() { "foo", "bar", "baz" };
+        private static readonly IList<int> SampleNumberCollection = new List<int>() { 1, 10, 100 };
+
+        public static readonly IEnumerable<object[]> ConvertInputs = new List<object[]>()
+        {
+            new object[] { SampleTextCollection, "", "foobarbaz" },
+            new object[] { SampleTextCollection, "-", "foo-bar-baz" },
+            new object[] { SampleTextCollection, ", ", "foo, bar, baz" },
+            new object[] { SampleNumberCollection, ", ", "1, 10, 100" },
+        };
+
+        [Theory]
+        [MemberData(nameof(ConvertInputs))]
+        public void Convert(IEnumerable collection, string separator, string expectedResult)
+        {
+            Assert.Equal(expectedResult, CallConvert(collection, separator));
+        }
+
+        private object CallConvert(IEnumerable collection, string separator)
+        {
+            return _sut.Convert(collection, null, separator, null);
+        }
+    }
+}
---
+++ toolkitcore/AWSToolkit/CommonUI/Converters/ConcatenateCollectionConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Amazon.AWSToolkit.CommonUI.Converters
+{
+    public class ConcatenateCollectionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is IEnumerable collection && parameter is string separator)
+            {
+                return string.Join<object>(separator, collection.Cast<object>());
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
