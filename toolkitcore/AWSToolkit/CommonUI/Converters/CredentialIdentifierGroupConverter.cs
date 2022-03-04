194f363e0def85b44fbc07167d8582047afcf3e1
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2022-03-04 09:06:51 -0800

    Create CredentialId selector, add to Credential Selection Dialog (#934)
    This change adds a new reusable user control for selecting from a list of `ICredentialIdentifier` objects. It is similar to an existing control for selecting from AccountViewModel objects (and leverages the same grouping logic - #933), and is intended for use in newer code, as we move away from the older heavier models.
    
    The change has been integrated into the credentials selection dialog (#931). The dialog also received a few styling adjustments at the same time.

---
+++ toolkitcore/AWSToolkit/CommonUI/Converters/CredentialIdentifierGroupConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+using Amazon.AWSToolkit.Credentials.Core;
+using Amazon.AWSToolkit.Credentials.Presentation;
+
+namespace Amazon.AWSToolkit.CommonUI.Converters
+{
+    /// <summary>
+    /// "Converts" <see cref="ICredentialIdentifier"/> to a grouping based on the credentials type.
+    /// </summary>
+    public class CredentialIdentifierGroupConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ICredentialIdentifier identifier)
+            {
+                return identifier.GetPresentationGroup();
+            }
+
+            return CredentialsIdentifierGroup.AdditionalCredentials;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
