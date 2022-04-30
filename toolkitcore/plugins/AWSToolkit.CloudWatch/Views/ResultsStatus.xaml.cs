495d796e1be7cb04318ad89e7aca567793499ab1
Shruti Sinha <44882001+shruti0085@users.noreply.github.com>
2022-04-29 19:09:32 -0700

    Create separate Results Status Control (#1017)

---
+++ toolkitcore/plugins/AWSToolkit.CloudWatch/Views/ResultsStatus.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Amazon.AWSToolkit.CloudWatch.Views
+{
+    /// <summary>
+    /// Indicates status of results eg. error, no results
+    /// </summary>
+    public partial class ResultsStatus : UserControl
+    {
+        public ResultsStatus()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ErrorMessageProperty =
+            DependencyProperty.Register(
+                nameof(ErrorMessage), typeof(string), typeof(ResultsStatus),
+                new PropertyMetadata(null));
+
+        public string ErrorMessage
+        {
+            get => (string) GetValue(ErrorMessageProperty);
+            set => SetValue(ErrorMessageProperty, value);
+        }
+    }
+}
