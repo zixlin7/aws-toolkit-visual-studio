e5b540ea0a7417b885b3b472cb6627350f1ff30b
Ralph Flora <floralph@amazon.com>
2022-07-06 21:36:19 -0700

    Add DataErrorInfo mixin (#1139)
    * Add DataErrorInfo mixin
    
    * Fix some issues with implemenation and add more tests.
    
    * Reverting field name to avoid confusion with the values it returns

---
+++ tests/AWSToolkit.Tests/CommonUI/DataErrorInfoTests.cs
@@ -0,0 +1,272 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+using Amazon.AWSToolkit.CommonUI;
+
+using Xunit;
+using Xunit.Sdk;
+
+namespace AWSToolkit.Tests.CommonUI
+{
+    public class DataErrorInfoTests
+    {
+        private readonly DataErrorInfo _sut;
+
+        private readonly IDataErrorInfo _sutAsIDataErrorInfo;
+
+        public DataErrorInfoTests()
+        {
+            _sut = new DataErrorInfo(this);
+            _sutAsIDataErrorInfo = _sut;
+        }
+
+        [Fact]
+        public void HasErrorsReturnsTrueWithErrorsAndFalseWithout()
+        {
+            Assert.False(_sut.HasErrors);
+            _sut.AddError("Test Error");
+            Assert.True(_sut.HasErrors);
+        }
+
+        [Fact]
+        public void HasErrorsReturnsFalseWhenHadErrorsButAllCleared()
+        {
+            Assert.False(_sut.HasErrors);
+
+            _sut.AddError("Test Error");
+
+            Assert.True(_sut.HasErrors);
+
+            _sut.ClearErrors();
+
+            Assert.False(_sut.HasErrors);
+        }
+
+        [Fact]
+        public void HasErrorsReturnsFalseWhenHadErrorsButAllRemoved()
+        {
+            const string error = "Error";
+
+            Assert.False(_sut.HasErrors);
+
+            _sut.AddError(error, null, "Property1", "Property2");
+
+            Assert.True(_sut.HasErrors);
+
+            _sut.RemoveError(error);
+
+            Assert.False(_sut.HasErrors);
+        }
+
+        [Fact]
+        public void GetErrorsAtEntityLevelWorksForNullEmpty()
+        {
+            const string error = "Entity Error";
+
+            Assert.False(_sut.HasErrors);
+
+            _sut.AddError(error);
+
+            Assert.True(_sut.HasErrors);
+
+            Assert.Collection(_sut.GetErrors(null).Cast<string>(), s => Assert.Equal(error, s));
+            Assert.Collection(_sut.GetErrors(string.Empty).Cast<string>(), s => Assert.Equal(error, s));
+        }
+
+        [Fact]
+        public void GetErrorsReturnsNoDuplicatesAtEntityLevel()
+        {
+            const string error = "Entity Error";
+
+            Assert.False(_sut.HasErrors);
+
+            _sut.AddError(error);
+            _sut.AddError(error, null);
+            _sut.AddError(error, null, null);
+            _sut.AddError(error, string.Empty);
+            _sut.AddError(error, null, string.Empty);
+
+            Assert.True(_sut.HasErrors);
+
+            Assert.Equal(error, _sutAsIDataErrorInfo.Error);
+            Assert.Equal(error, _sutAsIDataErrorInfo[null]);
+            Assert.Equal(error, _sutAsIDataErrorInfo[string.Empty]);
+
+            Assert.Single(_sut.GetErrors(null));
+            Assert.Collection(_sut.GetErrors(string.Empty).Cast<string>(), s => Assert.Equal(error, s));
+        }
+
+        [Fact]
+        public void GetErrorsReturnsNoDuplicatesAtPropertyLevel()
+        {
+            const string error = "Property Error";
+
+            Assert.False(_sut.HasErrors);
+
+            _sut.AddError(error, "Property1");
+            _sut.AddError(error, "Property1");
+            _sut.AddError(error, "Property2");
+
+            Assert.True(_sut.HasErrors);
+
+            AssertOnlyHasCrossPropertyError(error, "Property1", "Property2");
+        }
+
+        [Fact]
+        public void AddingCrossPropertyErrorsIsRetrievablePerProperty()
+        {
+            const string error = "Cross-property Error";
+
+            Assert.False(_sut.HasErrors);
+
+            _sut.AddError(error, "Property1", "Property2", "Property3");
+
+            Assert.True(_sut.HasErrors);
+
+            AssertOnlyHasCrossPropertyError(error, "Property1", "Property2", "Property3");
+        }
+
+        [Fact]
+        public void FailedAssertionsInCallbacksResultInFailedTests()
+        {
+            // This test doesn't directly validate DataErrorInfo, but rather confirms assumptions about how assertions behave
+            // in callbacks to give confidence in tests that utilize assertions in callbacks.
+
+            const string userMessage = "Can you believe it!?  This is the expected exception!  No doubt about it.  Nobody would throw a real exception with such a pointless message.";
+
+            _sut.ErrorsChanged += (sender, e) =>
+            {
+                Assert.True(false, userMessage);
+            };
+
+            var ex = Record.Exception(() => _sut.AddError("Error")) as TrueException;
+            Assert.NotNull(ex);
+            Assert.Equal(userMessage, ex.UserMessage);
+        }
+
+        [Fact]
+        public void OnErrorsChangedFires()
+        {
+            string expected = null;
+
+            _sut.ErrorsChanged += (sender, e) =>
+            {
+                Assert.Equal(this, sender);
+                Assert.Equal(expected, e.PropertyName);
+            };
+
+            Assert.False(_sut.HasErrors);
+
+            expected = "";
+            _sut.AddError("Error", null);
+
+            expected = "";
+            _sut.AddError("Error", string.Empty);
+
+            expected = "Property1";
+            _sut.AddError("Error", "Property1");
+
+            expected = "Property2";
+            _sut.AddError("Error", "Property2");
+
+            Assert.True(_sut.HasErrors);
+        }
+
+        [Fact]
+        public void OnErrorsChangedFiresForCrossPropertyErrors()
+        {
+            const string error = "Error";
+            var expected = new List<string>() { "Property1", "Property2", "Property3" };
+
+            _sut.ErrorsChanged += (sender, e) =>
+            {
+                Assert.Equal(this, sender);
+                Assert.Contains(e.PropertyName, expected);
+
+                expected.Remove(e.PropertyName);
+            };
+
+            Assert.False(_sut.HasErrors);
+
+            _sut.AddError(error, "Property1", "Property2", "Property3");
+
+            Assert.True(_sut.HasErrors);
+        }
+
+        [Fact]
+        public void RemoveErrorRemovesForSingleAndCrossPropertyErrors()
+        {
+            const string error = "Error";
+
+            Assert.False(_sut.HasErrors);
+
+            _sut.AddError(error, null, "Property1", "Property2", "Property3");
+
+            Assert.True(_sut.HasErrors);
+
+            AssertOnlyHasCrossPropertyError(error, string.Empty, "Property1", "Property2", "Property3");
+
+            _sut.RemoveError(error);
+
+            Assert.Equal(string.Empty, _sutAsIDataErrorInfo.Error);
+            Assert.Empty(_sut.GetErrors(null));
+            Assert.Empty(_sut.GetErrors("Property1"));
+            Assert.Empty(_sut.GetErrors("Property2"));
+            Assert.Empty(_sut.GetErrors("Property3"));
+
+            Assert.False(_sut.HasErrors);
+        }
+
+        [Fact]
+        public void ClearErrorsRemovesAllErrors()
+        {
+            const string error = "Error";
+
+            Assert.False(_sut.HasErrors);
+
+            _sut.AddError(error, null, "Property1", "Property2", "Property3");
+
+            Assert.True(_sut.HasErrors);
+
+            _sut.ClearErrors();
+
+            Assert.Empty(_sut.GetErrors(null));
+
+            Assert.False(_sut.HasErrors);
+        }
+
+        [Fact]
+        public void ClearErrorsRemovesErrorsForSpecificProperties()
+        {
+            const string error = "Error";
+
+            Assert.False(_sut.HasErrors);
+
+            _sut.AddError(error, null, "Property1", "Property2", "Property3");
+
+            Assert.True(_sut.HasErrors);
+
+            _sut.ClearErrors("Property1", "Property3");
+
+            Assert.Single(_sut.GetErrors(null));
+            Assert.Contains(error, _sut.GetErrors(string.Empty).OfType<string>());
+            Assert.Empty(_sut.GetErrors("Property1"));
+            Assert.Single(_sut.GetErrors("Property2"));
+            Assert.Contains(error, _sut.GetErrors("Property2").OfType<string>());
+            Assert.Empty(_sut.GetErrors("Property3"));
+
+            Assert.True(_sut.HasErrors);
+        }
+
+        private void AssertOnlyHasCrossPropertyError(object error, params string[] propertyNames)
+        {
+            foreach (var propertyName in propertyNames)
+            {
+                Assert.Single(_sut.GetErrors(propertyName));
+                Assert.Collection(_sut.GetErrors(propertyName).Cast<string>(), s => Assert.Equal(error, s));
+                Assert.Equal(error, _sutAsIDataErrorInfo[propertyName]);
+            }
+        }
+    }
+}
---
+++ toolkitcore/AWSToolkit/CommonUI/DataErrorInfo.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Amazon.AWSToolkit.CommonUI
+{
+    /// <summary>
+    /// Mixin for ViewModels and Entities to provide INotifyDataErrorInfo support.  IDataErrorInfo support is provided, but INotifyDataErrorInfo is recommended.
+    /// </summary>
+    /// <remarks>
+    /// Supports custom error objects, multiple errors per property, cross-property errors, and entity-level errors. Cross-property errors are
+    /// errors that affect multiple properties. You can associate these errors with one or all of the affected properties, or you can treat them
+    /// as entity-level errors. Entity-level errors are errors that either affect multiple properties or affect the entire entity without
+    /// affecting a particular property.
+    ///
+    /// IDataErrorInfo enables data entity classes to implement custom validation rules and expose validation results to the user interface.
+    /// You typically implement this interface to provide relatively simple validation logic. To provide asynchronous or server-side validation logic,
+    /// implement the INotifyDataErrorInfo interface instead. In general, new entity classes should implement INotifyDataErrorInfo for the added
+    /// flexibility instead of implementing IDataErrorInfo. The IDataErrorInfo support enables you to use many existing entity classes that are written
+    /// for the full.NET Framework.
+    ///
+    /// Implementation based on <see href="https://docs.microsoft.com/en-us/dotnet/api/system.componentmodel.inotifydataerrorinfo?view=netframework-4.7.2">
+    /// INotifyDataErrorInfo interface</see> documentation and the <see href="https://github.com/CommunityToolkit/dotnet/blob/main/CommunityToolkit.Mvvm/ComponentModel/ObservableValidator.cs">
+    /// Mvvm Toolkit's ObservableValidator</see> implementation of the INotifyDataErrorInfo interface.  Behaviors not clearly defined in the interface
+    /// documentation follow those of the ObservableValidator implementation.
+    /// </remarks>
+    /// <seealso href="https://social.technet.microsoft.com/wiki/contents/articles/19490.wpf-4-5-validating-data-in-using-the-inotifydataerrorinfo-interface.aspx"/>
+    public class DataErrorInfo : INotifyDataErrorInfo, IDataErrorInfo
+    {
+        private static readonly string EntityLevelErrorKey = string.Empty;
+
+        private readonly IDictionary<string, ISet<object>> _errorInfo = new Dictionary<string, ISet<object>>();
+
+        private readonly object _sender;
+
+        /// <summary>
+        /// Creates a new instance of DataErrorInfo.
+        /// </summary>
+        /// <param name="sender">Supplied as the sender in ErrorsChanged events.</param>
+        public DataErrorInfo(object sender)
+        {
+            _sender = sender;
+        }
+
+        #region INotifyDataErrorInfo
+
+        public bool HasErrors => _errorInfo.Any(pair => pair.Value.Any());
+
+        public IEnumerable GetErrors(string propertyName) => _errorInfo.TryGetValue(NormalizePropertyName(propertyName), out var error)
+            ? error : Enumerable.Empty<object>();
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        protected void RaiseErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(_sender, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        #region IDataErrorInfo
+
+        string IDataErrorInfo.this[string columnName] => GetErrors(columnName).OfType<object>().FirstOrDefault()?.ToString() ?? string.Empty;
+
+        string IDataErrorInfo.Error =>
+            _errorInfo.TryGetValue(EntityLevelErrorKey, out var values) && values.Any() ?
+                values.First()?.ToString() :
+                _errorInfo.Values.FirstOrDefault()?.FirstOrDefault()?.ToString()
+                ?? string.Empty;
+
+        #endregion
+
+        public void AddError(object error, params string[] propertyNames)
+        {
+            if (error == null)
+            {
+                return;
+            }
+
+            if (propertyNames == null || !propertyNames.Any())
+            {
+                propertyNames = new[] { EntityLevelErrorKey };
+            }
+
+            foreach (var propertyName in propertyNames.Select(NormalizePropertyName))
+            {
+                if (!_errorInfo.TryGetValue(propertyName, out var errors))
+                {
+                    // Once a property is added and given an errors set, don't ever remove it to simplify code,
+                    // avoid race conditions, and be consistent with ObservableValidator.
+                    errors = new HashSet<object>();
+                    _errorInfo[propertyName] = errors;
+                }
+
+                errors.Add(error);
+                RaiseErrorsChanged(propertyName);
+            }
+        }
+
+        public void RemoveError(object error)
+        {
+            if (error == null)
+            {
+                return;
+            }
+
+            foreach (var pair in _errorInfo)
+            {
+                if (pair.Value.Remove(error))
+                {
+                    RaiseErrorsChanged(pair.Key);
+                }
+            }
+        }
+
+        public void ClearErrors(params string[] propertyNames)
+        {
+            if (propertyNames == null || !propertyNames.Any())
+            {
+                propertyNames = _errorInfo.Keys.ToArray();
+            }
+
+            foreach (var propertyName in propertyNames.Select(NormalizePropertyName))
+            {
+                if (_errorInfo.TryGetValue(propertyName, out var errors) && errors.Any())
+                {
+                    errors.Clear();
+                    RaiseErrorsChanged(propertyName);
+                }
+            }
+        }
+
+        private static string NormalizePropertyName(string propertyName) => propertyName ?? EntityLevelErrorKey;
+    }
+}
