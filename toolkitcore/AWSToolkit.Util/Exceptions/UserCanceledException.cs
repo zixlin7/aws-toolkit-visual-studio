3b5ec08490ea0f5870c3771b96a2363c4c0e29ca
Shruti Sinha <44882001+shruti0085@users.noreply.github.com>
2022-07-12 16:04:08 -0700

    Handle user cancellation resolving credentials for CodeArtifact provider (#1148)

---
+++ toolkitcore/AWSToolkit.Util/Exceptions/UserCanceledException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Amazon.AWSToolkit.Exceptions
+{
+    public class UserCanceledException : Exception
+    {
+        public UserCanceledException(string message) : base(message) { }
+        public UserCanceledException(string message, Exception e) : base(message, e) { }
+    }
+}
