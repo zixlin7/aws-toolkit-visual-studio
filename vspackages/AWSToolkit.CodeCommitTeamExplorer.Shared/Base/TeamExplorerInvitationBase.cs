1317090c61f08602c04db6dd46d0ad41ce3cf482
Ralph Flora <floralph@amazon.com>
2022-08-23 08:14:59 -0700

    Remove TeamExplorerBase and use TeamExplorerServiceSectionBase instead (#1189)
    Removes TeamExplorerBase and most of the TeamExplorerInvitationSectionBase implementation so that the latter extends TeamExplorerServiceSectionBase from the Microsoft.TeamFoundation.Controls assembly.  This simplifies the code base and reuses mostly boilerplate implementation from the TFS SDK.

--- vspackages/AWSToolkit.CodeCommitTeamExplorer.Shared/Base/TeamExplorerInvitationBase.cs
+++ vspackages/AWSToolkit.CodeCommitTeamExplorer.Shared/Base/TeamExplorerInvitationBase.cs
@@ -1,89 +1,35 @@
 ﻿using System;
-using Microsoft.TeamFoundation.Controls;
+using System.Windows;
+using System.Windows.Media;
+
+using Microsoft.TeamFoundation.Controls.WPF.TeamExplorer;
 
 namespace Amazon.AWSToolkit.CodeCommitTeamExplorer.Base
 {
-    public class TeamExplorerInvitationBase : TeamExplorerBase, ITeamExplorerServiceInvitation
+    public class TeamExplorerInvitationBase : TeamExplorerServiceInvitationBase
     {
         public const string TeamExplorerInvitationSectionId = "C2443FCC-6D62-4D31-B08A-C4DE70109C7F";
 
-        private bool _canConnect;
-        private bool _canSignUp;
-        private string _connectLabel;
-        private string _description;
-        private object _icon;
-        private bool _isVisible;
-        private string _name;
-        private string _provider;
-        private string _signUpLabel;
-
-        /// <summary>
-        /// Triggers the login flow
-        /// </summary>
-        public virtual void Connect() { }
-
-        /// <summary>
-        /// Starts the sign up process online
-        /// </summary>
-        public virtual void SignUp() { }
-
-        public bool CanConnect
+        public override void Dispose()
         {
-            get => _canConnect;
-            set { _canConnect = value; NotifyPropertyChanged(); }
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
 
-        public bool CanSignUp
+        protected virtual void Dispose(bool disposing)
         {
-            get => _canSignUp;
-            set { _canSignUp = value; NotifyPropertyChanged(); }
         }
 
-        public string ConnectLabel
+        protected static DrawingBrush CreateDrawingBrush(ImageSource imageSource)
         {
-            get => _connectLabel;
-            set { _connectLabel = value; NotifyPropertyChanged(); }
-        }
+            var drawing = new DrawingGroup();
+            drawing.Children.Add(new GeometryDrawing
+            {
+                Brush = new ImageBrush(imageSource),
+                Geometry = new RectangleGeometry(new Rect(new Size(imageSource.Width, imageSource.Height)))
+            });
 
-        public string Description
-        {
-            get => _description;
-            set { _description = value; NotifyPropertyChanged(); }
-        }
-
-        public object Icon
-        {
-            get => _icon;
-            set { _icon = value; NotifyPropertyChanged(); }
-        }
-
-        public bool IsVisible
-        {
-            get => _isVisible;
-            set { _isVisible = value; NotifyPropertyChanged(); }
-        }
-
-        public string Name
-        {
-            get => _name;
-            set { _name = value; NotifyPropertyChanged(); }
-        }
-
-        public string Provider
-        {
-            get => _provider;
-            set { _provider = value; NotifyPropertyChanged(); }
-        }
-
-        public string SignUpLabel
-        {
-            get => _signUpLabel;
-            set { _signUpLabel = value; NotifyPropertyChanged(); }
-        }
-
-        public virtual void Initialize(IServiceProvider serviceProvider)
-        {
-            TeamExplorerServiceProvider = serviceProvider;
+            return new DrawingBrush(drawing);
         }
     }
 }
