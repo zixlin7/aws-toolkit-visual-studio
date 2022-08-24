d68bdec097e9a7d2ee8384ccbfaee87648cbb1a3
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2022-08-24 14:20:08 -0700

    Update deploy tool to 1.2.4 (#1194)
    This change updates the deploy tool to the latest release (1.2.4 - https://github.com/aws/aws-dotnet-deploy/releases/tag/1.2.4). This fixes the problem where users could not deploy to Fargate in a region where there is no default VPC (aws/aws-toolkit-visual-studio#271).
    
    This change was run against the integration tests. I also saw that I could select Fargate as a deployment choice in a region without a default VPC (but I didn't try deploying it).

--- vspackages/Publishing/PublishToAws.Shared/Install/InstallOptionsFactory.cs
+++ vspackages/Publishing/PublishToAws.Shared/Install/InstallOptionsFactory.cs
@@ -5,7 +5,7 @@ namespace Amazon.AWSToolkit.Publish.Install
 {
     public class InstallOptionsFactory
     {
-        private const string VersionRange = "1.1.15";
+        private const string VersionRange = "1.2.4";
 
         public static InstallOptions Create(IToolkitHostInfo toolkitHostInfo)
         {
