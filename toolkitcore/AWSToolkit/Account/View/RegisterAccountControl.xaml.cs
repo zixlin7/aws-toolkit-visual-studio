zixlin7/aws-toolkit-visual-studio

commit 3bcd8b2735ea3d19b010b46dd348f4cf437b8ce2

Update help link in Credentials dialog (#1193)
The add/edit credentials dialog had a link to a help page that no longer exists (https://aws.amazon.com/developers/access-keys/). The dialog has been updated to link to a page in the Toolkit User Guide instead. Also, the link has been removed, in favor of using more standard dialog help conventions. Now the dialog has a help icon in the titlebar, and a help button in the footer, which will open up the user guide's credentials page. Pressing F1 in this dialog also does the same thing.

This was accomplished by leveraging the VS SDK's DialogWindow callbacks for providing a help action. I introduced `IHelpHandler` as a way of providing help without bloating `IAWSToolkitControl` with more concerns.

I also performed a little cleanup in the RegisterAccountModel file.

toolkitcore/AWSToolkit/Account/View/RegisterAccountControl.xaml
@@ -36,7 +36,6 @@
                 <RowDefinition Height="auto"/>
                 <RowDefinition Height="auto"/>
                 <RowDefinition Height="*"/>
-                <RowDefinition Height="auto"/>
             </Grid.RowDefinitions>
 
             <Label Grid.Column="0" Grid.Row="0" Target="{Binding ElementName=_ctlDisplayName}" 
@@ -131,24 +130,11 @@
             <WrapPanel Grid.Column="2" Grid.Row="10" Margin="0 6 0 0" Visibility="{Binding IsPartitionEnabled, Converter={StaticResource InverseBooleanToVisibilityConverter}}">
                 <TextBlock>Don't see your region?</TextBlock>
                 <TextBlock Margin="6 0 0 0">
-                <Hyperlink NavigateUri="https://aws.amazon.com/developers/access-keys/" RequestNavigate="OnRequestRegions">
+                <Hyperlink RequestNavigate="OnRequestRegions">
                     <TextBlock Text="Show more regions" />
                 </Hyperlink></TextBlock>
 
             </WrapPanel>
-
-            <WrapPanel Grid.Column="0" Grid.Row="11" Grid.ColumnSpan="3" Margin="0 9 0 0" VerticalAlignment="Bottom">
-                <TextBlock>Account information can found at:</TextBlock>
-                <TextBlock Margin="3 0 0 0">
-                    <Hyperlink NavigateUri="https://aws.amazon.com/developers/access-keys/" RequestNavigate="onRequestNavigate">
-                        <TextBlock 
-                            Name="_ctlAWSLink" 
-                            Text="https://aws.amazon.com/developers/access-keys/"
-                            />
-                    </Hyperlink>
-                </TextBlock>
-            </WrapPanel>
-
         </Grid>
     </ScrollViewer>
 </commonui:BaseAWSControl>

toolkitcore/AWSToolkit/Account/View/RegisterAccountControl.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,6 +8,7 @@ using System.Windows.Navigation;
 using Amazon.AWSToolkit.Account.Controller;
 using Amazon.AWSToolkit.CommonUI;
 using Amazon.AWSToolkit.Settings;
+using Amazon.AWSToolkit.Shared;
 
 namespace Amazon.AWSToolkit.Account.View
 {
@@ -17,7 +17,7 @@ namespace Amazon.AWSToolkit.Account.View
     /// </summary>
     public partial class RegisterAccountControl : BaseAWSControl
     {
-        RegisterAccountController _controller;
+        private readonly RegisterAccountController _controller;
 
         public RegisterAccountControl(RegisterAccountController controller)
         {
@@ -83,13 +83,6 @@ namespace Amazon.AWSToolkit.Account.View
         {
             Unloaded -= OnUnloaded;
             _controller.Model.PropertyChanged -= OnPropertyChanged;
-        }
-
-        void onRequestNavigate(object sender, RequestNavigateEventArgs e)
-        {
-            string url = this._ctlAWSLink.Text;
-            Process.Start(new ProcessStartInfo(url));
-            e.Handled = true;
         }
 
         private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
@@ -174,5 +167,7 @@ namespace Amazon.AWSToolkit.Account.View
         {
             this._controller.Model.AccessKey = this._ctlAccessKey.Password;
         }
+
+        public override IHelpHandler GetHelpHandler() => _controller.Model;
     }
 }

vspackages/AwsToolkitPackage.Shared/FirstRun/View/FirstRunControl.xaml
@@ -172,7 +172,7 @@
 
                             <WrapPanel Grid.Column="0" Grid.Row="0" Grid.ColumnSpan="2" Margin="4,4,0,0">
                                 <TextBlock TextWrapping="WrapWithOverflow" Style="{DynamicResource awsTextBlockBaseStyle}"> AWS Toolkit will assume that these credentials are valid for the region us-east-1. If you are working with other regions, 
-                                    <Hyperlink NavigateUri="https://aws.amazon.com/developers/access-keys/" RequestNavigate="OnRequestCreateDialog">
+                                    <Hyperlink RequestNavigate="OnRequestCreateDialog">
                                         <TextBlock Style="{DynamicResource awsUrlTextFieldStyle}" 
                                                    Text="click here"
                                         />

toolkitcore/AWSToolkit/Account/Controller/RegisterAccountController.cs
@@ -17,7 +17,7 @@ namespace Amazon.AWSToolkit.Account.Controller
 {
     public class RegisterAccountController
     {
-        private RegisterAccountModel _model;
+        private readonly RegisterAccountModel _model;
         protected RegisterAccountControl _control;
         protected bool DefaultProfileNameInUse;
         protected ActionResults _results;
@@ -26,7 +26,7 @@ namespace Amazon.AWSToolkit.Account.Controller
         public RegisterAccountController(ToolkitContext toolkitContext)
         {
             ToolkitContext = toolkitContext;
-            this._model = new RegisterAccountModel(ToolkitContext.RegionProvider);
+            this._model = new RegisterAccountModel(toolkitContext);
         }
 
         public RegisterAccountModel Model => this._model;

toolkitcore/AWSToolkit/Account/Model/RegisterAccountModel.cs
@@ -5,46 +5,32 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 
+using Amazon.AWSToolkit.Context;
 using Amazon.AWSToolkit.Regions;
 using Amazon.AWSToolkit.Regions.Manifest;
+using Amazon.AWSToolkit.Shared;
 using Amazon.AWSToolkit.Util;
 
 using log4net;
 
 namespace Amazon.AWSToolkit.Account.Model
 {
-    public class RegisterAccountModel : INotifyPropertyChanged
+    public class RegisterAccountModel : INotifyPropertyChanged, IHelpHandler
     {
-        private static ILog LOGGER = LogManager.GetLogger(typeof(RegisterAccountModel));
-        private readonly IRegionProvider _regionProvider;
+        private const string CredentialsHelpUrl = "https://docs.aws.amazon.com/toolkit-for-visual-studio/latest/user-guide/credentials.html";
+
+        private static readonly ILog LOGGER = LogManager.GetLogger(typeof(RegisterAccountModel));
+        private readonly ToolkitContext _toolkitContext;
         private ToolkitRegion _region;
         private Partition _partition;
-        private IList<Partition> _partitions = new List<Partition>();
+        private readonly IList<Partition> _partitions;
         private ObservableCollection<ToolkitRegion> _regions = new ObservableCollection<ToolkitRegion>();
         private bool _isPartitionEnabled = false;
 
-        public RegisterAccountModel(IRegionProvider regionProvider)
+        public RegisterAccountModel(ToolkitContext toolkitContext)
         {
-            this._regionProvider = regionProvider;
-            this._partitions = this._regionProvider?.GetPartitions();
-        }
-
-        public RegisterAccountModel()
-        {
-        }
-
-        public RegisterAccountModel(string credentialId, string displayName, string accessKey, string secretKey)
-        {
-            this.CredentialId = credentialId;
-            this.DisplayName = displayName;
-            this.AccessKey = accessKey;
-        }
-
-        public RegisterAccountModel(Guid uniqueKey, string displayName, string accessKey, string secretKey)
-        {
-            this.UniqueKey = uniqueKey;
-            this.DisplayName = displayName;
-            this.AccessKey = accessKey;
+            _toolkitContext = toolkitContext;
+            _partitions = _toolkitContext.RegionProvider?.GetPartitions();
         }
 
         public Guid UniqueKey { get; set; }
@@ -190,15 +176,20 @@ namespace Amazon.AWSToolkit.Account.Model
         /// </summary>
         public void ShowRegionsForPartition(string partitionId)
         {
-            var regions = _regionProvider.GetRegions(partitionId);
+            var regions = _toolkitContext.RegionProvider.GetRegions(partitionId);
 
-            Regions = new ObservableCollection<ToolkitRegion>(regions.Where(r=> !_regionProvider.IsRegionLocal(r.Id)).OrderBy(r => r.DisplayName));
+            Regions = new ObservableCollection<ToolkitRegion>(regions.Where(r=> !_toolkitContext.RegionProvider.IsRegionLocal(r.Id)).OrderBy(r => r.DisplayName));
         }
 
         public override string ToString()
         {
             return string.Format("DisplayName: {0}, AccessKey: {1}, SecretKey {2}", this.ProfileName, this.AccessKey,
                 this.SecretKey);
+        }
+
+        public void OnHelp()
+        {
+            _toolkitContext.ToolkitHost.OpenInBrowser(CredentialsHelpUrl, false);
         }
 
         public void Validate()

toolkitcore/AWSToolkit/CommonUI/BaseAWSControl.cs
@@ -89,6 +89,8 @@ namespace Amazon.AWSToolkit.CommonUI
             return true;
         }
 
+        public virtual IHelpHandler GetHelpHandler() => null;
+
         public virtual void RefreshInitialData(object initialData)
         {
         }

toolkitcore/AWSToolkit/Shared/IAWSToolkitControl.cs
@@ -36,5 +36,10 @@ namespace Amazon.AWSToolkit.Shared
         string AcceptButtonText { get; }
 
         string RejectButtonText { get; }
+
+        /// <summary>
+        /// Returns null if the dialog doesn't have an associated Help action.
+        /// </summary>
+        IHelpHandler GetHelpHandler();
     }
 }

toolkitcore/AWSToolkit/Shared/IHelpHandler.cs
@@ -1,0 +1,10 @@
+﻿namespace Amazon.AWSToolkit.Shared
+{
+    /// <summary>
+    /// Provides an action to handle a help gesture
+    /// </summary>
+    public interface IHelpHandler
+    {
+        void OnHelp();
+    }
+}

commit add1f1a2a7a3b1bc3dddb1d422f17d593ba7f03b

Fix hyperlinks that do not do anything (#1248)
This change fixes a bug introduced in #1193 where hyperlinks in the following locations would no longer do anything:
- edit credentials dialog: the link to show the partitions dropdown
- first run dialog: the link to open the add credentials dialog

Apparently Hyperlink objects without a NavigateUrl attribute will not fire RequestNavigate when clicked. Rather than add a placeholder url, I switched to the Click event in these places.

toolkitcore/AWSToolkit/Account/View/RegisterAccountControl.xaml
@@ -130,7 +130,7 @@
             <WrapPanel Grid.Column="2" Grid.Row="10" Margin="0 6 0 0" Visibility="{Binding IsPartitionEnabled, Converter={StaticResource InverseBooleanToVisibilityConverter}}">
                 <TextBlock>Don't see your region?</TextBlock>
                 <TextBlock Margin="6 0 0 0">
-                <Hyperlink RequestNavigate="OnRequestRegions">
+                <Hyperlink Click="OnRequestRegions">
                     <TextBlock Text="Show more regions" />
                 </Hyperlink></TextBlock>
 

toolkitcore/AWSToolkit/Account/View/RegisterAccountControl.xaml.cs
@@ -74,7 +74,7 @@ namespace Amazon.AWSToolkit.Account.View
             return true;
         }
 
-        void OnRequestRegions(object sender, RequestNavigateEventArgs e)
+        void OnRequestRegions(object sender, RoutedEventArgs e)
         {
             this._controller.Model.IsPartitionEnabled = true;
         }

vspackages/AwsToolkitPackage.Shared/FirstRun/View/FirstRunControl.xaml
@@ -172,7 +172,7 @@
 
                             <WrapPanel Grid.Column="0" Grid.Row="0" Grid.ColumnSpan="2" Margin="4,4,0,0">
                                 <TextBlock TextWrapping="WrapWithOverflow" Style="{DynamicResource awsTextBlockBaseStyle}"> AWS Toolkit will assume that these credentials are valid for the region us-east-1. If you are working with other regions, 
-                                    <Hyperlink RequestNavigate="OnRequestCreateDialog">
+                                    <Hyperlink Click="OnRequestCreateDialog">
                                         <TextBlock Style="{DynamicResource awsUrlTextFieldStyle}" 
                                                    Text="click here"
                                         />

vspackages/AwsToolkitPackage.Shared/FirstRun/View/FirstRunControl.xaml.cs
@@ -133,7 +133,7 @@ namespace Amazon.AWSToolkit.VisualStudio.FirstRun.View
             e.Handled = true;
         }
 
-        private void OnRequestCreateDialog(object sender, RequestNavigateEventArgs e)
+        private void OnRequestCreateDialog(object sender, RoutedEventArgs e)
         {
             var toolkitContext = this._controller.ToolkitContext;
             var command = new RegisterAccountController(toolkitContext);