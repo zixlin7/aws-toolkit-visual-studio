a2481c7d3a46999431a322510dadc69d4a786fbe
Shruti Sinha <44882001+shruti0085@users.noreply.github.com>
2022-11-23 17:31:31 -0800

    Update CodeCatalyst endpoint to prod (#1318)
    Reverts PR #1284 change to point to gamma back to prod now that the API model is fixed.

--- toolkitcore/AWSToolkit/Urls/ServiceUrls.cs
+++ toolkitcore/AWSToolkit/Urls/ServiceUrls.cs
@@ -2,9 +2,6 @@
 {
     public static class ServiceUrls
     {
-        // TODO: Swap back to public once the API changes have settled down IDE-9135
-        // public const string CodeCatalyst = "https://public.api.quokka.codes";
-
-        public const string CodeCatalyst = "https://public.api-gamma.quokka.codes/";
+        public const string CodeCatalyst = "https://public.codecatalyst.global.api.aws";
     }
 }
