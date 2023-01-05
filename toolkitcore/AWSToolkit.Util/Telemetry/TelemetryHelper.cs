15c521675ca11fbc47ca1edde381e5c14c165205
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2023-01-05 09:48:46 -0800

    Emit metric when creating DynamoDB Tables (#1356)
    This change adds metrics for DynamoDB table creation. The error code (not the error message) is emitted with failure cases.
    
    To support this change, I enhanced ActionResults with utility methods for creating cancelled and failure states. I also added an Exception field, which can be interrogated.

---
+++ toolkitcore/AWSToolkit.Util/Telemetry/TelemetryHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Amazon.Runtime;
+
+namespace Amazon.AWSToolkit.Telemetry
+{
+    /// <summary>
+    /// Misc Telemetry related functions
+    /// </summary>
+    public static class TelemetryHelper
+    {
+        /// <summary>
+        /// Returns a value to use as the reason field for a metric.
+        /// Not intended to surface user-identifiable details.
+        /// </summary>
+        public static string GetMetricsReason(Exception exception)
+        {
+            switch (exception)
+            {
+                case null:
+                    return null;
+                case AmazonServiceException awsException:
+                    return awsException.ErrorCode;
+                default:
+                    return "Unknown";
+            }
+        }
+    }
+}
