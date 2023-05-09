de8d25090ebe7bc1fb422b8fb8c431c646cc4a4a
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2023-05-09 10:32:08 -0700

    Add test to guard against CodeCatalyst service client regression (c72fb08) (#1442)
    This change adds a test that guards against the credentials engine receiving an "Unable to get IAM security credentials from EC2 Instance Metadata Service" exception when resolving BuilderID tokens with the CodeCatalyst service client.

---
+++ tests/AWSToolkit.Tests/Credentials/Core/IdentityResolverTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Amazon.AWSToolkit.Credentials.Core;
+using Amazon.Runtime;
+
+using Xunit;
+
+namespace AWSToolkit.Tests.Credentials.Core
+{
+    public class IdentityResolverTests
+    {
+        private readonly IdentityResolver _sut = new IdentityResolver();
+
+        [Fact]
+        public async Task GetAwsIdAsync_DoesNotThrowServiceException()
+        {
+            // This test guards against AmazonCodeCatalystClient raising a AmazonServiceException in the event that the service client attempts
+            // to resolve IAM based AWSCredentials. If that were to happen, an "Unable to get IAM security credentials from EC2 Instance Metadata Service."
+            // exception is raised.
+            // We know this client should be using a bearer token. We're passing it a fake token provider, which will return a
+            // "No Token found.  Operation requires a Bearer token." AmazonClientException
+            await Assert.ThrowsAsync<AmazonClientException>(() => _sut.GetAwsIdAsync(new FakeAwsTokenProvider(), CancellationToken.None));
+        }
+    }
+}
