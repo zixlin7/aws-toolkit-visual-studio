76ec63dde830370a8e4be726159c483f30b467ec
Ralph Flora <floralph@amazon.com>
2023-05-31 10:17:32 -0700

    Copy original auth classes as basis for updates. (#1453)

---
+++ toolkitcore/AWSToolkit/Account/Controller/RegisterAccountController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+
+using Amazon.AWSToolkit.Navigator;
+using Amazon.AWSToolkit.Account.View;
+using Amazon.AWSToolkit.Account.Model;
+using Amazon.AWSToolkit.Credentials.Core;
+using Amazon.AWSToolkit.Credentials.Utils;
+
+using System.Threading;
+
+using Amazon.AWSToolkit.Regions;
+using Amazon.AWSToolkit.Context;
+using System.Linq;
+using System.Windows;
+
+using Amazon.AWSToolkit.Telemetry;
+using Amazon.AWSToolkit.Telemetry.Model;
+using Amazon.AwsToolkit.Telemetry.Events.Core;
+using Amazon.AwsToolkit.Telemetry.Events.Generated;
+
+namespace Amazon.AWSToolkit.Account.Controller
+{
+    public class RegisterAccountController
+    {
+        private readonly RegisterAccountModel _model;
+        protected RegisterAccountControl _control;
+        protected bool DefaultProfileNameInUse;
+        protected ActionResults _results;
+        protected readonly ToolkitContext ToolkitContext;
+
+        public RegisterAccountController(ToolkitContext toolkitContext)
+        {
+            ToolkitContext = toolkitContext;
+            this._model = new RegisterAccountModel(toolkitContext);
+        }
+
+        public RegisterAccountModel Model => this._model;
+
+        /// <summary>
+        /// Overload to record telemetry when the base class is invoked
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public ActionResults Execute(BaseMetricSource source)
+        {
+            ActionResults actionResults = null;
+
+            void Invoke() => actionResults = Execute();
+
+            void Record(ITelemetryLogger _) => RecordAddMetric(actionResults, source);
+
+            ToolkitContext.TelemetryLogger.InvokeAndRecord(Invoke, Record);
+            return actionResults;
+        }
+
+        public virtual ActionResults Execute()
+        {
+            this.Model.StorageLocationVisibility = System.Windows.Visibility.Visible;
+            this._control = new RegisterAccountControl(this);
+            this.LoadModel();
+            CustomizeControl(this._control);
+            if (ToolkitContext.ToolkitHost.ShowInModalDialogWindow(this._control, MessageBoxButton.OKCancel))
+            {
+                return this._results;
+            }
+       
+            return ActionResults.CreateCancelled().WithSuccess(false);
+        }
+
+        protected virtual void CustomizeControl(RegisterAccountControl control)
+        {
+        }
+
+        protected virtual void LoadModel()
+        {
+            // if this is the first account, seed the display name to 'default'
+            // like the first-run experience
+            var identifiers = ToolkitContext.CredentialManager.GetCredentialIdentifiers();
+            if(identifiers.Count == 0)
+            {
+                _model.ProfileName = "default";
+            }
+            else
+            {
+                var defaultCredential = identifiers.FirstOrDefault(x=>string.Equals(x.ProfileName, "default"));
+                if (defaultCredential != null)
+                {
+                    DefaultProfileNameInUse = true;
+                }
+            }
+        
+            this.Model.InitializeDefaultPartition();
+        }
+
+        public bool PromptToUseDefaultName => !DefaultProfileNameInUse;
+
+        public virtual void Persist()
+        {
+            ICredentialIdentifier identifier = null;
+            ToolkitRegion region = null;
+            ManualResetEvent mre = new ManualResetEvent(false);
+            EventHandler<EventArgs> HandleCredentialUpdate = (sender, args) =>
+            {
+                var ide = ToolkitContext.CredentialManager.GetCredentialIdentifierById(identifier?.Id);
+                if (ide != null && region != null)
+                {
+                    mre.Set();
+                    ToolkitContext.ConnectionManager.ChangeConnectionSettings(identifier, region);
+                }
+            };
+
+            try
+            {
+                this.Model.UniqueKey = Guid.NewGuid();
+
+                if (this.Model.SelectedStorageType == StorageTypes.DotNetEncryptedStore)
+                {
+                    identifier = new SDKCredentialIdentifier(this.Model.ProfileName.Trim());
+                }
+                else
+                {
+                    identifier = new SharedCredentialIdentifier(this.Model.ProfileName.Trim());
+                }
+
+                this.Model.CredentialId = identifier?.Id;
+                this.Model.ProfileName = identifier?.ProfileName;
+                this.Model.DisplayName = identifier?.DisplayName;
+                region = this.Model.Region;
+                var properties = new ProfileProperties
+                {
+                    Name = this.Model.ProfileName.Trim(),
+                    AccessKey = this.Model.AccessKey?.Trim(),
+                    SecretKey = this.Model.SecretKey?.Trim(),
+                    UniqueKey = this.Model.UniqueKey.ToString(),
+                    Region = this.Model.Region?.Id
+                };
+
+                ToolkitContext.CredentialManager.CredentialManagerUpdated += HandleCredentialUpdate;
+
+                // create profile ensures profile has unique key and registers it
+                ToolkitContext.CredentialSettingsManager.CreateProfile(identifier, properties);
+                
+
+                mre.WaitOne(2000);
+                this._results = new ActionResults().WithSuccess(true).WithFocalname(this.Model.ProfileName);
+            }
+            catch(Exception e)
+            {
+                this._results = ActionResults.CreateFailed(e).WithFocalname(this.Model.ProfileName);
+            }
+            finally
+            {
+                ToolkitContext.CredentialManager.CredentialManagerUpdated -= HandleCredentialUpdate;
+            }
+        }
+
+        private void RecordAddMetric(ActionResults actionResults, BaseMetricSource source)
+        {
+            ToolkitContext.TelemetryLogger.RecordAwsModifyCredentials(new AwsModifyCredentials()
+            {
+                AwsAccount = ToolkitContext.ConnectionManager?.ActiveAccountId ?? MetadataValue.NotSet,
+                AwsRegion = MetadataValue.NotApplicable,
+                Result = actionResults.AsTelemetryResult(),
+                CredentialModification = CredentialModification.Add,
+                Source = source.Location,
+                ServiceType = source.Service,
+                Reason = TelemetryHelper.GetMetricsReason(actionResults?.Exception)
+            });
+        }
+    }
+}
---
+++ vspackages/AwsToolkitPackage.Shared/FirstRun/View/FirstRunControl.xaml.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Navigation;
+using System.Windows.Input;
+
+using Amazon.AWSToolkit.Account.Controller;
+using Amazon.AWSToolkit.Navigator;
+using Amazon.AWSToolkit.VisualStudio.FirstRun.Controller;
+using Amazon.AWSToolkit.VisualStudio.FirstRun.Model;
+using log4net;
+using Amazon.AWSToolkit.Settings;
+
+namespace Amazon.AWSToolkit.VisualStudio.FirstRun.View
+{
+    /// <summary>
+    /// Interaction logic for FirstRunControl.xaml
+    /// </summary>
+    public partial class FirstRunControl
+    {
+        readonly FirstRunController _controller;
+
+        readonly ILog LOGGER = LogManager.GetLogger(typeof(FirstRunControl));
+
+        private bool _saveFirstRunModel = true;
+
+        public FirstRunControl()
+            : this(null)
+        {
+        }
+
+        public FirstRunControl(FirstRunController controller)
+        {
+            this._controller = controller;
+            this.DataContext = this._controller.Model;
+            this._controller.Model.PropertyChanged += OnPropertyChanged;
+            this.Loaded += FirstRunControl_Loaded;
+
+            InitializeComponent();
+            ThemeUtil.ThemeChange += ThemeUtilOnThemeChange;
+
+            // match the logo header 'text' to the theme on startup
+            ThemeUtilOnThemeChange(null, null);
+            Unloaded += OnUnloaded;
+        }
+
+        private void FirstRunControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            ToolkitSettings.Instance.HasUserSeenFirstRunForm = true;
+
+            this.Loaded -= FirstRunControl_Loaded;
+        }
+
+        private void ThemeUtilOnThemeChange(object sender, EventArgs eventArgs)
+        {
+            var logo = ThemeUtil.GetLogoImageSource("logo_aws");
+            if (logo != null)
+            {
+                _ctlLogoImage.Source = logo;
+            }
+        }
+
+        public override string Title => "AWS Getting Started";
+
+        public override string UniqueId => "AWSGettingStarted";
+
+        private FirstRunModel Model => _controller.Model;
+
+        public override void OnEditorOpened(bool success)
+        {
+            _controller.RecordAwsHelpQuickstartMetric(success);
+        }
+
+        private void OnClickSaveAndClose(object sender, RoutedEventArgs e)
+        {
+            Cursor = Cursors.Wait;
+            var autoExit = true;
+
+            if (_saveFirstRunModel)
+            {
+                ActionResults result = null;
+                try
+                {
+                    Model.Save();
+                    result = new ActionResults().WithSuccess(true);
+                }
+                catch (Exception ex)
+                {
+                    autoExit = false;
+                    LOGGER.Error("Error during save of credentials", ex);
+                    result = ActionResults.CreateFailed(ex);
+                }
+                finally
+                {
+                    _controller.RecordAwsAddCredentialsMetric(result);
+                }
+            }
+
+            if (autoExit)
+            {
+                Microsoft.VisualStudio.Shell.ThreadHelper.JoinableTaskFactory.Run(async () =>
+                {
+                    if (Model.OpenAWSExplorerOnClosing)
+                    {
+                        await _controller.ShowAwsExplorerAsync();
+                    }
+
+                    _controller.CloseEditor(this);
+                });
+            }
+
+            Cursor = Cursors.Arrow;
+        }
+
+        private void ImportAWSCredentials_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var csvFilename = ShowFileOpenDialog("Import AWS Credentials from Csv File");
+                if (csvFilename != null)
+                {
+                    Model.AwsCredentialsFromCsv(csvFilename);
+                }
+            }
+            catch(Exception ex)
+            {
+                LOGGER.Error("Error importing AWS credentials", ex);
+            }
+        }
+
+        private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            LaunchBrowserToUrl(e.Uri);
+            e.Handled = true;
+        }
+
+        private void OnRequestCreateDialog(object sender, RoutedEventArgs e)
+        {
+            var toolkitContext = this._controller.ToolkitContext;
+            var command = new RegisterAccountController(toolkitContext);
+            var results  = command.Execute(MetricSources.GettingStartedMetricSource.GettingStarted);
+            if (results.Success)
+            {
+                _saveFirstRunModel = false;
+                SaveAndCloseButton.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+            }
+        }
+
+        private void LaunchBrowserToUrl(Uri uri)
+        {
+            try
+            {
+                _controller.OpenInBrowser(uri.ToString());
+            }
+            catch (Exception ex)
+            {
+                LOGGER.Error("Failed to launch process to go to endpoint " + uri, ex);
+            }
+        }
+
+        private static string ShowFileOpenDialog(string title)
+        {
+            var dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                DefaultExt = ".csv",
+                Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*",
+                CheckPathExists = true,
+                Title = title
+            };
+
+            var result = dlg.ShowDialog();
+            if (result.GetValueOrDefault())
+                return dlg.FileName;
+
+            return null;
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            Unloaded -= OnUnloaded;
+            _controller.Model.PropertyChanged -= OnPropertyChanged;
+        }
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(_controller.Model.AccessKey))
+            {
+                UpdatePassword(AccessKey, _controller.Model.AccessKey);
+            }
+
+            if (e.PropertyName == nameof(_controller.Model.SecretKey))
+            {
+                UpdatePassword(SecretKey, _controller.Model.SecretKey);
+            }
+        }
+
+        private void UpdatePassword(PasswordBox passwordBox, string password)
+        {
+            if (passwordBox.Password != password)
+            {
+                passwordBox.Password = password;
+            }
+        }
+
+        private void AccessKey_OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            this._controller.Model.AccessKey = this.AccessKey.Password;
+        }
+
+        private void SecretKey_OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            this._controller.Model.SecretKey = this.SecretKey.Password;
+        }
+    }
+
+}
