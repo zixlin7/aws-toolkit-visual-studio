0a892929c95d4d7aed0708b1556ab8aeefeb62d3
Ralph Flora <floralph@amazon.com>
2023-06-12 16:16:06 -0700

    Add RegionSelector control for Profile Region and SSO Region (#1458)
    * Add RegionSelector control for Profile Region and SSO Region
    
    * Remove unnecessary usings in ToolkitContextFixture
    
    * Optimize removal of local region in RegionSelectorViewModel

---
+++ toolkitcore/AWSToolkit/CommonUI/CredentialProfiles/RegionSelector.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Amazon.AWSToolkit.CommonUI.CredentialProfiles
+{
+    public partial class RegionSelector : UserControl
+    {
+        public RegionSelector()
+        {
+            InitializeComponent();
+        }
+    }
+}
