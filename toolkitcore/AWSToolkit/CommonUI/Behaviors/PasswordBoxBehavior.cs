d38c87c7b393e7a4f66d30d224baab29f3aac6be
Ralph Flora <floralph@amazon.com>
2023-06-13 14:57:46 -0700

    Add ProfileSubform, SsoProfileSubform, and StaticProfileSubform (#1459)
    * Add ProfileSubform, SsoProfileSubform, and StaticProfileSubform
    
    * Updates based on rev1 feedback

---
+++ toolkitcore/AWSToolkit/CommonUI/Behaviors/PasswordBoxBehavior.cs
@@ -0,0 +1,77 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Amazon.AWSToolkit.CommonUI.Behaviors
+{
+    public static class PasswordBoxBehavior
+    {
+        public static readonly DependencyProperty BindPasswordProperty = DependencyProperty.RegisterAttached(
+            "BindPassword",
+            typeof(bool),
+            typeof(PasswordBoxBehavior),
+            new PropertyMetadata(BindPassword_PropertyChanged));
+
+        public static bool GetBindPassword(DependencyObject d)
+        {
+            return (bool) d.GetValue(BindPasswordProperty);
+        }
+
+        public static void SetBindPassword(DependencyObject d, bool value)
+        {
+            d.SetValue(BindPasswordProperty, value);
+        }
+
+        private static void BindPassword_PropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is PasswordBox passwordBox)
+            {
+                if ((bool) e.NewValue)
+                {
+                    passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+                }
+                else
+                {
+                    passwordBox.PasswordChanged -= PasswordBox_PasswordChanged;
+                }
+            }
+        }
+
+        private static void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox passwordBox && passwordBox.Password != GetPassword(passwordBox))
+            {
+                SetPassword(passwordBox, passwordBox.Password);
+            }
+        }
+
+        public static readonly DependencyProperty PasswordProperty = DependencyProperty.RegisterAttached(
+            "Password",
+            typeof(string),
+            typeof(PasswordBoxBehavior),
+            new FrameworkPropertyMetadata()
+            {
+                BindsTwoWayByDefault = true,
+                DefaultUpdateSourceTrigger = UpdateSourceTrigger.LostFocus,
+                PropertyChangedCallback = Password_PropertyChanged
+            });
+
+        public static string GetPassword(DependencyObject d)
+        {
+            return (string) d.GetValue(PasswordProperty);
+        }
+
+        public static void SetPassword(DependencyObject d, string value)
+        {
+            d.SetValue(PasswordProperty, value);
+        }
+
+        private static void Password_PropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is PasswordBox passwordBox && e.NewValue is string newPassword && passwordBox.Password != newPassword)
+            {
+                passwordBox.Password = newPassword;
+            }
+        }
+    }
+}
---
+++ toolkitcore/AWSToolkit/CommonUI/CredentialProfiles/SsoProfileSubform.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Amazon.AWSToolkit.Credentials.Utils;
+
+namespace Amazon.AWSToolkit.CommonUI.CredentialProfiles
+{
+    public partial class SsoProfileSubform : ProfileSubform
+    {
+        public SsoProfileSubform()
+        {
+            InitializeComponent();
+        }
+
+        public override CredentialType CredentialType => CredentialType.SsoProfile;
+    }
+}
