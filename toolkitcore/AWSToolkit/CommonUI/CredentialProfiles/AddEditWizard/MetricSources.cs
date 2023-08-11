5420f07d25ab7266babb91025474bf6be9e3d198
Ralph Flora <floralph@amazon.com>
2023-08-11 14:53:19 -0700

    Add revamp base view models and services (#1522)

---
+++ toolkitcore/AWSToolkit/CommonUI/CredentialProfiles/AddEditWizard/MetricSources.cs
@@ -0,0 +1,14 @@
+﻿using Amazon.AWSToolkit.Telemetry.Model;
+
+namespace Amazon.AWSToolkit.CommonUI.CredentialProfiles.AddEditWizard
+{
+    public class MetricSources
+    {
+        public class AddEditProfileWizardViewModelMetricSource : BaseMetricSource
+        {
+            public static readonly BaseMetricSource AddEditProfileWizard = new AddEditProfileWizardViewModelMetricSource("AddEditProfileWizard");
+
+            private AddEditProfileWizardViewModelMetricSource(string location) : base(null, location) { }
+        }
+    }
+}
---
+++ toolkitcore/AWSToolkit/CommonUI/CredentialProfiles/AddEditWizard/Services/IAddEditProfileWizard.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Amazon.AWSToolkit.Credentials.Utils;
+
+namespace Amazon.AWSToolkit.CommonUI.CredentialProfiles.AddEditWizard.Services
+{
+    public enum WizardStep
+    {
+        Configuration,
+        SsoConnecting,
+        SsoConnected
+    }
+
+    public enum CredentialFileType
+    {
+        Sdk,
+        Shared
+    }
+
+    public class CredentialsFileOpenedEventArgs : EventArgs
+    {
+        public static readonly CredentialsFileOpenedEventArgs Empty = new CredentialsFileOpenedEventArgs();
+    }
+
+    public interface IAddEditProfileWizard
+    {
+        WizardStep CurrentStep { get; set; }
+
+        bool InProgress { get; set; }
+
+        void OpenCredentialsFile();
+
+        Task SaveAsync(ProfileProperties profileProperties, CredentialFileType fileType, bool changeConnectionSettings = true);
+
+        event EventHandler<CredentialsFileOpenedEventArgs> CredentialsFileOpened;
+
+        event EventHandler<ConnectionSettingsChangeArgs> ConnectionSettingsChanged;
+    }
+}
