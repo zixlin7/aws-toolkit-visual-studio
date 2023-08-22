59e8479c67916ff6d880a463f0de6ce73764a456
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2023-08-22 12:22:39 -0700

    IToolkitContextProvider - a way for MEF components to gain access to ToolkitContext (#1536)
    Visual Studio activates MEF components separately from Packages. Most of the Toolkit extension is created through the Package initialization, however we have MEF components that need to access objects that are created by the Toolkit Package (and will have more MEF components in the future). This can lead to race conditions and requires singletons and synchronization code to coordinate and access shared state. One of the objects needed by all code is the ToolkitContext.
    
    It turns out [IComponentModel](https://learn.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.componentmodelhost.icomponentmodel?view=visualstudiosdk-2022) offers a way for Package code to access MEF components, instantiating new objects if needed.
    
    This change introduces ToolkitContextProvider - a MEF component that "provides" the ToolkitContext into other MEF components. MEF components can import IToolkitContextProvider and query it for the ToolkitContext. The Toolkit Package will apply the ToolkitContext into IToolkitContextProvider after it has been created.
    
    This change does not integrate IToolkitContextProvider into any existing MEF components. Those changes would increase the size of this change, and will be made as separate changes at a later time.

---
+++ vspackages/AwsToolkitPackage.Shared/Services/ToolkitContextProvider.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Amazon.AWSToolkit.Context;
+
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Threading;
+
+namespace Amazon.AWSToolkit.VisualStudio.Services
+{
+    /// <summary>
+    /// Allows us to depencency inject the ToolkitContext into systems.
+    /// This is primarily intended for use with MEF components, which
+    /// can be activated independently from the main AWS Toolkit package.
+    ///
+    /// MEF components can Import a IToolkitContextProvider and wait
+    /// for this object to have a ToolkitContext as part of their initialization routine.
+    /// </summary>
+    [Export(typeof(IToolkitContextProvider))]
+    internal class ToolkitContextProvider : IToolkitContextProvider, IDisposable
+    {
+        private static readonly object _onInitializedLock = new object();
+
+        private Action _onInitialized;
+
+        private bool _isDisposed;
+        private ToolkitContext _toolkitContext;
+        private readonly ManualResetEvent _waitForContextSync = new ManualResetEvent(false);
+
+        [ImportingConstructor]
+        public ToolkitContextProvider()
+        {
+        }
+
+        public void Initialize(ToolkitContext toolkitContext)
+        {
+            lock (_onInitializedLock)
+            {
+                if (HasToolkitContext())
+                {
+                    throw new InvalidOperationException("ToolkitContext was already initialized");
+                }
+
+                _toolkitContext = toolkitContext;
+            }
+
+            _waitForContextSync.Set();
+
+            _onInitialized?.Invoke();
+            _onInitialized = null;
+        }
+
+        public bool HasToolkitContext()
+        {
+            return _toolkitContext != null;
+        }
+
+        public ToolkitContext GetToolkitContext()
+        {
+            return _toolkitContext ?? throw new InvalidOperationException("ToolkitContext Provider has not been initialized");
+        }
+
+        public Task<ToolkitContext> WaitForToolkitContextAsync()
+        {
+            return WaitForToolkitContextAsync(-1);
+        }
+
+        public async Task<ToolkitContext> WaitForToolkitContextAsync(int timeoutMs)
+        {
+            // Avoid the risk of deadlocking on the UI thread
+            // TODO : IDE-11472 : Check for UI Thread (requires tests that juggle threads)
+            // ThreadHelper.ThrowIfOnUIThread();
+
+            await TaskScheduler.Default;
+
+            // Calling HasToolkitContext is an optimization to avoid native code calls from WaitOne
+            return HasToolkitContext() || _waitForContextSync.WaitOne(timeoutMs)
+                ? GetToolkitContext()
+                : throw new TimeoutException("ToolkitContext was not available.");
+        }
+
+        public void RegisterOnInitializedCallback(Action callback)
+        {
+            lock (_onInitializedLock)
+            {
+                if (HasToolkitContext())
+                {
+                    callback.Invoke();
+                }
+                else
+                {
+                    _onInitialized += callback;
+                }
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    _waitForContextSync.Dispose();
+                }
+
+                _isDisposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
