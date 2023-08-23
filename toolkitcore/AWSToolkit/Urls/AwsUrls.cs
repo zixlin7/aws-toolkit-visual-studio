1fe37c509481776b2473c952bef8da5c364cd273
Ralph Flora <floralph@amazon.com>
2023-08-23 12:21:06 -0700

    Update auth UX revamp tooltip text (#1544)
    * Update auth UX revamp tooltip text
    
    * Move hard-coded URLs to AwsUrls and add NavigateUriFromStatic to make assignment in XAML easier.

--- toolkitcore/AWSToolkit/Urls/AwsUrls.cs
+++ toolkitcore/AWSToolkit/Urls/AwsUrls.cs
@@ -3,13 +3,16 @@
     public class AwsUrls
     {
         public const string CodeCatalystUserGuidePatConcept = "https://docs.aws.amazon.com/codecatalyst/latest/userguide/concepts.html#personal-access-token-concept";
-        public const string DotNetOnAws = "https://aws.amazon.com/developer/language/net/";
-        public const string DotNetOnAwsCommunity = "https://aws.amazon.com/developer/language/net/net-community/";
-        public const string UserGuide = "https://docs.aws.amazon.com/toolkit-for-visual-studio/latest/user-guide/welcome.html";
         public const string DeployLambdaDocs = "https://docs.aws.amazon.com/toolkit-for-visual-studio/latest/user-guide/lambda-cli-publish.html";
         public const string DeployBeanstalkDocs = "https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/create_deploy_NET.html";
         public const string DevBlog = "https://aws.amazon.com/blogs/developer/category/net/";
+        public const string DotNetOnAws = "https://aws.amazon.com/developer/language/net/";
+        public const string DotNetOnAwsCommunity = "https://aws.amazon.com/developer/language/net/net-community/";
+        public const string IamIdC = "https://docs.aws.amazon.com/singlesignon/latest/userguide/what-is.html";
         public const string PrivacyPolicy = "https://aws.amazon.com/privacy/";
         public const string TelemetryDisclosure = "https://docs.aws.amazon.com/sdkref/latest/guide/support-maint-idetoolkits.html";
+        public const string UserGuide = "https://docs.aws.amazon.com/toolkit-for-visual-studio/latest/user-guide/welcome.html";
+        public const string UserGuideAuth = "https://docs.aws.amazon.com/toolkit-for-visual-studio/latest/user-guide/credentials.html";
+        public const string UserGuideAuthIamCreds = "https://docs.aws.amazon.com/toolkit-for-visual-studio/latest/user-guide/keys-profiles-credentials.html";
     }
 }
