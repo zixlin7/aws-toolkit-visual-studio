0739c46195c4fcc3fc49dfb6a456250201396174
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2023-08-24 10:35:00 -0700

    Start of CodeWhisperer language client implementation (#1543)
    This change contains the start of the CodeWhisperer language client implementation. Additional future language clients have been kept in mind. The common code lives in ToolkitLspClient, and CodeWhispererClient contains handling specific to this first LSP integration.
    
    Many portions of the client have been earmarked with TODO comments, which will be handled in upcoming work.
    
    The language server has been set up to be associated with C# files. The server starts up and performs the initialization handshake, but does not do anything else. Future work is required before the language server is seen doing anything in the IDE:
    - Toolkit needs to provide bearer tokens to the server
    - Toolkit needs to make calls to generate code recommendations, then surface the responses in the IDE

---
+++ vspackages/CodeWhisperer/AwsToolkit.CodeWhisperer.Shared/Lsp/Settings/CodeWhispererLspSettingsRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+
+using AwsToolkit.VsSdk.Common.Settings.CodeWhisperer;
+
+namespace Amazon.AwsToolkit.CodeWhisperer.Lsp.Settings
+{
+    /// <summary>
+    /// Repository to retrieve and store CodeWhisperer language server related settings.
+    /// Gives MEF components some separation from the actual CodeWhispererSettings I/O.
+    ///
+    /// Settings are backed by Visual Studio's own settings system.
+    /// </summary>
+    [Export(typeof(ICodeWhispererLspSettingsRepository))]
+    public class CodeWhispererLspSettingsRepository : ICodeWhispererLspSettingsRepository
+    {
+        [ImportingConstructor]
+        public CodeWhispererLspSettingsRepository()
+        {
+        }
+
+        public Task<CodeWhispererSettings> GetAsync()
+        {
+            return CodeWhispererSettings.GetLiveInstanceAsync();
+        }
+
+        public void Save(CodeWhispererSettings settings)
+        {
+            settings.Save();
+        }
+    }
+}
