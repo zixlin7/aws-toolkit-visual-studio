c7db2222d56c2846b5fb6e1ee2770f821afd4075
Ralph Flora <floralph@amazon.com>
2023-08-25 13:15:43 -0700

    Add auth_addedConnections metrics (#1550)

--- vspackages/AwsToolkitPackage.Shared/GettingStarted/MetricSources.cs
+++ vspackages/AwsToolkitPackage.Shared/GettingStarted/MetricSources.cs
@@ -6,7 +6,12 @@ namespace Amazon.AWSToolkit.VisualStudio.GettingStarted
     {
         public class GettingStartedMetricSource : BaseMetricSource
         {
+            // This should be used when the Getting Started page is opened from the Extensions main menu item
             public static readonly BaseMetricSource GettingStarted = new GettingStartedMetricSource("AwsGettingStarted");
+
+            // This should be used on the first run of the toolkit when the Getting Started page is opened from
+            // AWSToolkitPackage.ShowFirstRun
+            public static readonly BaseMetricSource FirstStartup = new GettingStartedMetricSource("firstStartup");
 
             private GettingStartedMetricSource(string location) : base(null, location)
             {
