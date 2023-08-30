e8de288b1f3a50b57dab88fc9bbc7ecf32e44cc0
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2023-08-30 11:25:45 -0700

    Set up a project to place CodeWhisperer tests (#1554)
    We need a place where CodeWhisperer tests (non-integration tests) can live. This change creates the test project, and seeds it with the first testable component.
    
    Unfortunately there is a problem with the third party dependency that will be used by that first testable component, so this change stubs out the actual package reference (commented with a TODO), and will be a follow up change. This gets the boilerplate changes to support the test project out of the way.
    
    I didn't use one of the existing VS test projects:
    - CodeWhisperer is supported in VS 2022 (not VS 2019). This avoids the need to ifdef swaths of test code.
    - these projects already spider out and take a dependency most of the repo in order to compile and run. This allows us to test the CodeWhisperer project's code faster.

---
+++ vspackages/CodeWhisperer/AwsToolkit.CodeWhisperer.Shared/Lsp/Credentials/JwtJsonMapper.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Amazon.AwsToolkit.CodeWhisperer.Lsp.Credentials
+{
+    /// <summary>
+    /// Defines JSON serialization behavior that is used with JWT encoding.
+    /// We want payload property names to use camelCase by default, rather than .NET's PascalCase.
+    /// Otherwise recipients (language servers) will not locate the fields they are looking for.
+    /// </summary>
+    // TODO : Add a reference to a strongly signed version of the jose-jwt NuGet package
+    //internal class JwtJsonMapper : IJsonMapper
+    internal class JwtJsonMapper
+    {
+        private readonly JsonSerializerSettings _jsonSerializerSettings = new JsonSerializerSettings()
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+        };
+
+        public string Serialize(object obj)
+        {
+            return JsonConvert.SerializeObject(obj, _jsonSerializerSettings);
+        }
+
+        public T Parse<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json, _jsonSerializerSettings);
+        }
+    }
+}
