844dc7c92c14f024fe759979a9ec52738c3df842
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2023-09-21 10:43:06 -0700

    Add support for testing code that uses JoinableTaskFactory (#1587)
    This change sets up supporting test code and dependencies for testing code that uses the JoinableTaskFactory and/or the VSSDK.
    
    The change is based on guidance in https://github.com/Microsoft/vs-threading/blob/main/doc/testing_vs.md and https://github.com/microsoft/vssdktestfx/blob/main/src/Microsoft.VisualStudio.Sdk.TestFramework.Xunit/README.md . Basically we use a Fixture, which stubs in a selection of central VS SDK objects. One of the things that this sets up is code that ThreadHelper requires, which means we should be able to validate UI/background thread use in tests.
    
    This change only supports VS 2022 and newer. The plan is to use it in CodeWhisperer work first, which is also only supporting VS 2022 and newer.

---
+++ tests/AWSToolkit.Tests.Common.VS.Shared/VsTestFramework/VsMockCollection.cs
@@ -0,0 +1,42 @@
+﻿#if VS2022_OR_LATER
+using Microsoft.VisualStudio.Sdk.TestFramework;
+
+using Xunit;
+
+namespace AwsToolkit.Vs.Tests.VsTestFramework
+{
+    /// <summary>
+    /// Collection fixture providing access to mocked VS services in unit tests.
+    /// VS 2022 is the minimum supported version to use this.
+    /// </summary>
+    /// <remarks>
+    /// This collection helps write tests around code that use JoinableTaskFactory and portions of the VS SDK.
+    /// Without this, tests that use JoinableTaskFactory will fail with an "assembly not found" message.
+    /// This collection allows tests to work with ThreadHelper.JoinableTaskContext.
+    ///
+    /// To use,
+    /// - add a Collection annotation to your test class
+    ///   [Collection(VsMockCollection.CollectionName)]
+    /// - pass in the required fixture(s) to the ctor
+    /// - call the GlobalServiceProvider object's Reset method.
+    ///   this stubs in some VS SDK objects.
+    /// 
+    /// The use of a collection fixture is how the vssdktestfx library prescribes implementing this functionality.  While there 
+    /// is a similar MockedVS class in the library, the toolkit uses its own class as it doesn't use the build constants defined 
+    /// in the <see href="https://github.com/microsoft/vssdktestfx/blob/main/src/Microsoft.VisualStudio.Sdk.TestFramework.Xunit/contentFiles/MockedVS.cs">MockedVS</see> class. 
+    ///
+    /// Collection fixtures do carry the risk of maintaining state between tests for all classes in the test collection.  
+    /// Tests in a collection cannot be run in parallel. 
+    /// </remarks>
+    /// <seealso href="https://github.com/microsoft/vssdktestfx/blob/main/src/Microsoft.VisualStudio.Sdk.TestFramework.Xunit/README.md">Using CollectionAttribute for VS mocked services</seealso>
+    /// <seealso href="https://github.com/Microsoft/vs-threading/blob/main/doc/testing_vs.md">Using JoinableTaskFactory in unit tests</seealso>
+    /// <seealso href="https://xunit.net/docs/shared-context">XUnit Shared Context between Tests</seealso>
+    [CollectionDefinition(CollectionName)]
+    public class VsMockCollection :
+        ICollectionFixture<GlobalServiceProvider>,
+        ICollectionFixture<MefHostingFixture>
+    {
+        public const string CollectionName = nameof(VsMockCollection);
+    }
+}
+#endif
