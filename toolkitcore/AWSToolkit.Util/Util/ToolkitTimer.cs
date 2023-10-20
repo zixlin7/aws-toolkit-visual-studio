14ee1f2accf11aefc2721042b8eae20bc1c8af33
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2023-10-20 15:34:42 -0700

    Update ToolkitTimer to encapsulate System.Timers.Timer instead of extend it (#1617)
    This change updates IToolkitTimer and the ToolkitTimer implementation to be a freestanding Timer implementation that can be stubbed in tests without a reliance on System.Timers.Timer.
    
    The existing implementation has not been used yet, but will be in an upcoming change. The issue that comes with exposure to System.Timers.Timer is that the ElapsedEventArgs class only has an internal constructor. So with a little encapsulation, we still have a drop in replacement to Timer that gives us better code testing flexibility.

--- toolkitcore/AWSToolkit.Util/Util/ToolkitTimer.cs
+++ toolkitcore/AWSToolkit.Util/Util/ToolkitTimer.cs
@@ -1,12 +1,62 @@
-﻿using System.Timers;
+﻿using System;
+using System.Timers;
 
 namespace Amazon.AWSToolkit.Util
 {
     /// <summary>
-    /// Extends System.Timers.Timer to fit IToolkitTimer
+    /// Encapsulates <see cref="System.Timers.Timer"/>
     /// </summary>
-    public class ToolkitTimer : Timer, IToolkitTimer
+    public class ToolkitTimer : IToolkitTimer
     {
-        
+        private readonly Timer _timer = new Timer();
+
+        public event EventHandler<ToolkitTimerElapsedEventArgs> Elapsed;
+
+        public bool AutoReset
+        {
+            get => _timer.AutoReset;
+            set => _timer.AutoReset = value;
+        }
+
+        public double Interval
+        {
+            get => _timer.Interval;
+            set => _timer.Interval = value;
+        }
+
+        public ToolkitTimer()
+        {
+            _timer.Elapsed += OnInternalTimerElapsed;
+        }
+
+        private void OnInternalTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            Elapsed?.Invoke(sender, new ToolkitTimerElapsedEventArgs(e.SignalTime));
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _timer.Elapsed -= OnInternalTimerElapsed;
+                _timer?.Dispose();
+            }
+        }
     }
 }
