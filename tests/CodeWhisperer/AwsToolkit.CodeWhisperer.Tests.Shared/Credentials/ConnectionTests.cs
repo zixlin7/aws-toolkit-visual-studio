6450c4f1f10d2fe960777d92e0919ec145592392
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2023-10-25 13:20:07 -0700

    Auto-refresh tokens for CodeWhisperer (#1620)
    This change sets up the CodeWhisperer Connection component to continuously refresh tokens after Sign-In. This way, the user can continuously use CodeWhisperer features without having to re sign-in every hour.
    
    Refreshes only succeed if they do not require user interaction (like performing the SSO login flow). If the system is unable to refresh the token, the CodeWhisperer connection is moved to the expired state, and then the user is signed out. In a future change, the expired state will cause the Toolkit to display a banner, where the user is informed that their connection has expired.

--- tests/CodeWhisperer/AwsToolkit.CodeWhisperer.Tests.Shared/Credentials/ConnectionTests.cs
+++ tests/CodeWhisperer/AwsToolkit.CodeWhisperer.Tests.Shared/Credentials/ConnectionTests.cs
@@ -1,16 +1,24 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 using Amazon.AwsToolkit.CodeWhisperer.Credentials;
 using Amazon.AwsToolkit.CodeWhisperer.Lsp.Clients;
 using Amazon.AwsToolkit.CodeWhisperer.Tests.Lsp.Clients;
+using Amazon.AwsToolkit.CodeWhisperer.Tests.TestUtilities;
+using Amazon.AwsToolkit.VsSdk.Common.Tasks;
 using Amazon.AWSToolkit.Context;
 using Amazon.AWSToolkit.Credentials.Core;
 using Amazon.AWSToolkit.Credentials.Sono;
 using Amazon.AWSToolkit.Credentials.Utils;
 using Amazon.AWSToolkit.Regions;
 using Amazon.AWSToolkit.Tests.Common.Context;
+using Amazon.AWSToolkit.Tests.Common.Time;
+using Amazon.AWSToolkit.Util;
 
 using FluentAssertions;
+
+using Microsoft.VisualStudio.Sdk.TestFramework;
+using Microsoft.VisualStudio.Shell;
 
 using Xunit;
 
@@ -20,8 +28,12 @@ namespace Amazon.AwsToolkit.CodeWhisperer.Tests.Credentials
     {
         public ICredentialIdentifier CredentialIdPromptResponse;
 
-        public StubConnection(IToolkitContextProvider toolkitContextProvider, ICodeWhispererLspClient codeWhispererLspClient)
-            : base(toolkitContextProvider, codeWhispererLspClient)
+        public StubConnection(
+            IToolkitContextProvider toolkitContextProvider,
+            ICodeWhispererLspClient codeWhispererLspClient,
+            ToolkitJoinableTaskFactoryProvider taskFactoryProvider,
+            IToolkitTimer timer)
+            : base(toolkitContextProvider, codeWhispererLspClient, taskFactoryProvider, timer)
         {
         }
 
@@ -31,10 +43,13 @@ namespace Amazon.AwsToolkit.CodeWhisperer.Tests.Credentials
         }
     }
 
-    public class ConnectionTests
+    [Collection(VsMockCollection.CollectionName)]
+    public class ConnectionTests : IDisposable
     {
         private readonly ToolkitContextFixture _toolkitContextFixture = new ToolkitContextFixture();
+        private readonly ToolkitJoinableTaskFactoryProvider _taskFactoryProvider;
         private readonly FakeCodeWhispererClient _codeWhispererClient = new FakeCodeWhispererClient();
+        private readonly FakeToolkitTimer _timer = new FakeToolkitTimer();
 
         private readonly ICredentialIdentifier _sampleCredentialId = FakeCredentialIdentifier.Create("AwsBuilderId");
         private readonly FakeTokenProvider _tokenProvider = new FakeTokenProvider("secret-token-123");
@@ -55,8 +70,12 @@ namespace Amazon.AwsToolkit.CodeWhisperer.Tests.Credentials
 
         private readonly StubConnection _sut;
 
-        public ConnectionTests()
+        public ConnectionTests(GlobalServiceProvider serviceProvider)
         {
+            serviceProvider.Reset();
+
+            _taskFactoryProvider = new ToolkitJoinableTaskFactoryProvider(ThreadHelper.JoinableTaskContext);
+
             var toolkitCredentials = new ToolkitCredentials(_sampleCredentialId, _tokenProvider);
             _toolkitContextFixture.SetupGetToolkitCredentials(toolkitCredentials);
 
@@ -64,7 +83,7 @@ namespace Amazon.AwsToolkit.CodeWhisperer.Tests.Credentials
             _toolkitContextFixture.DefineCredentialIdentifiers(new[] { _sampleCredentialId });
             _toolkitContextFixture.DefineCredentialProperties(_sampleCredentialId, _sampleProfileProperties);
 
-            _sut = new StubConnection(_toolkitContextFixture.ToolkitContextProvider, _codeWhispererClient);
+            _sut = new StubConnection(_toolkitContextFixture.ToolkitContextProvider, _codeWhispererClient, _taskFactoryProvider, _timer);
         }
 
         [Fact]
@@ -83,6 +102,118 @@ namespace Amazon.AwsToolkit.CodeWhisperer.Tests.Credentials
             await _sut.SignInAsync();
 
             _codeWhispererClient.CredentialsProtocol.TokenPayload.Token.Should().Be(_tokenProvider.Token);
+        }
+
+        [Fact]
+        public async Task SignOutAsync()
+        {
+            // set up: Sign in with a token expiring in 6 hours
+            const int tokenDurationHours = 6;
+            _tokenProvider.ExpiresAt = DateTime.UtcNow.AddHours(tokenDurationHours);
+
+            _sut.CredentialIdPromptResponse = _sampleCredentialId;
+            await _sut.SignInAsync();
+
+            // act
+            await _sut.SignOutAsync();
+
+            _sut.Status.Should().Be(ConnectionStatus.Disconnected);
+            _timer.IsStarted.Should().BeFalse();
+            _codeWhispererClient.CredentialsProtocol.TokenPayload.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task LongLivedTokenRefreshesFiveMinutesBeforeExpiration()
+        {
+            // set up: Sign in will occur with a token expiring in 6 hours
+            const int tokenDurationHours = 6;
+            _tokenProvider.ExpiresAt = DateTime.UtcNow.AddHours(tokenDurationHours);
+
+            _sut.CredentialIdPromptResponse = _sampleCredentialId;
+
+            // act
+            await _sut.SignInAsync();
+
+            _timer.IsStarted.Should().BeTrue();
+
+            // Should refresh "five minutes before expiration"
+            // We test with a generous range in order to avoid false positives
+            var expectedMinInterval = new TimeSpan(0, tokenDurationHours - 1, 53, 0).TotalMilliseconds;
+            var expectedMaxInterval = new TimeSpan(0, tokenDurationHours - 1, 55, 0).TotalMilliseconds;
+
+            _timer.Interval.Should().BeInRange(expectedMinInterval, expectedMaxInterval);
+        }
+
+        [Fact]
+        public async Task ShortLivedTokenRefreshesInOneMinute()
+        {
+            // set up: Sign in will occur with a token expiring in 4 minutes (eg: sooner than the system's 5 minute buffer)
+            const int tokenDurationMinutes = 4;
+            _tokenProvider.ExpiresAt = DateTime.UtcNow.AddMinutes(tokenDurationMinutes);
+
+            _sut.CredentialIdPromptResponse = _sampleCredentialId;
+
+            // act
+            await _sut.SignInAsync();
+
+            _timer.IsStarted.Should().BeTrue();
+
+            // Should refresh "a minute from now"
+            // We test with a generous range in order to avoid false positives
+            var expectedMinInterval = new TimeSpan(0, 0, 0, 55).TotalMilliseconds;
+            var expectedMaxInterval = new TimeSpan(0, 0, 1, 0).TotalMilliseconds;
+
+            _timer.Interval.Should().BeInRange(expectedMinInterval, expectedMaxInterval);
+        }
+
+        [Fact]
+        public async Task ImminentTokenExpirationRefreshesOnExpiration()
+        {
+            // set up: Sign in will occur with a token expiring extremely soon (sooner than the system's smallest refresh window of 1 minute)
+            const int tokenDurationSeconds = 20;
+            _tokenProvider.ExpiresAt = DateTime.UtcNow.AddSeconds(tokenDurationSeconds);
+
+            _sut.CredentialIdPromptResponse = _sampleCredentialId;
+
+            // act
+            await _sut.SignInAsync();
+
+            _timer.IsStarted.Should().BeTrue();
+
+            // Should refresh "when the token expires"
+            // We test with a generous range in order to avoid false positives
+            var expectedMinInterval = new TimeSpan(0, 0, 0, tokenDurationSeconds - 4).TotalMilliseconds;
+            var expectedMaxInterval = new TimeSpan(0, 0, 0, tokenDurationSeconds + 1).TotalMilliseconds;
+
+            _timer.Interval.Should().BeInRange(expectedMinInterval, expectedMaxInterval);
+        }
+
+        [Fact]
+        public async Task ExpiredTokenRefreshesImmediately()
+        {
+            // set up: Sign in will occur with a token that has expired (this is more to exercise the handling logic than sign in)
+            _tokenProvider.ExpiresAt = DateTime.UtcNow.AddMinutes(-1);
+
+            _sut.CredentialIdPromptResponse = _sampleCredentialId;
+
+            // act
+            await _sut.SignInAsync();
+
+            _timer.IsStarted.Should().BeTrue();
+
+            // Should refresh "three seconds from now"
+            // We test with a generous range in order to avoid false positives
+            var expectedMinInterval = new TimeSpan(0, 0, 0, 1).TotalMilliseconds;
+            var expectedMaxInterval = new TimeSpan(0, 0, 0, 3).TotalMilliseconds;
+
+            _timer.Interval.Should().BeInRange(expectedMinInterval, expectedMaxInterval);
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+            _sut?.Dispose();
+            _taskFactoryProvider?.Dispose();
         }
     }
 }
--- vspackages/CodeWhisperer/AwsToolkit.CodeWhisperer.Shared/Credentials/Connection.cs
+++ vspackages/CodeWhisperer/AwsToolkit.CodeWhisperer.Shared/Credentials/Connection.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.ComponentModel.Composition;
+using System.Linq;
 using System.Threading.Tasks;
 
 using Amazon.AwsToolkit.CodeWhisperer.Lsp.Clients;
 using Amazon.AwsToolkit.CodeWhisperer.Lsp.Credentials.Models;
+using Amazon.AwsToolkit.VsSdk.Common.Tasks;
 using Amazon.AWSToolkit.Context;
 using Amazon.AWSToolkit.Credentials.Core;
+using Amazon.AWSToolkit.Credentials.Sono;
+using Amazon.AWSToolkit.Credentials.Utils;
+using Amazon.AWSToolkit.Regions;
+using Amazon.AWSToolkit.Util;
+using Amazon.Runtime;
 
 using log4net;
 
@@ -15,21 +22,45 @@ namespace Amazon.AwsToolkit.CodeWhisperer.Credentials
     /// Handles the Connection state and operations for CodeWhisperer features.
     /// </summary>
     [Export(typeof(IConnection))]
-    internal class Connection : IConnection
+    internal class Connection : IConnection, IDisposable
     {
         private static readonly ILog _logger = LogManager.GetLogger(typeof(Connection));
 
         private readonly IToolkitContextProvider _toolkitContextProvider;
 
         private readonly IToolkitLspClient _codeWhispererLspClient;
+        private readonly ToolkitJoinableTaskFactoryProvider _taskFactoryProvider;
 
         private ICredentialIdentifier _signedInCredentialIdentifier;
+        private ToolkitRegion _signedInCredentialsSsoRegion;
+        private DateTime? _tokenExpiresAt;
+        private readonly IToolkitTimer _tokenRefreshTimer;
 
         [ImportingConstructor]
-        public Connection(IToolkitContextProvider toolkitContextProvider, ICodeWhispererLspClient codeWhispererLspClient)
+        public Connection(
+            IToolkitContextProvider toolkitContextProvider,
+            ICodeWhispererLspClient codeWhispererLspClient,
+            ToolkitJoinableTaskFactoryProvider taskFactoryProvider)
+        : this(toolkitContextProvider, codeWhispererLspClient, taskFactoryProvider, new ToolkitTimer())
+        {
+        }
+
+        /// <summary>
+        /// Constructor overload for testing purposes
+        /// </summary>
+        internal Connection(
+            IToolkitContextProvider toolkitContextProvider,
+            ICodeWhispererLspClient codeWhispererLspClient,
+            ToolkitJoinableTaskFactoryProvider taskFactoryProvider,
+            IToolkitTimer tokenRefreshTimer)
         {
             _toolkitContextProvider = toolkitContextProvider;
             _codeWhispererLspClient = codeWhispererLspClient;
+            _taskFactoryProvider = taskFactoryProvider;
+
+            _tokenRefreshTimer = tokenRefreshTimer;
+            _tokenRefreshTimer.AutoReset = false;
+            _tokenRefreshTimer.Elapsed += OnTokenRefreshTimerElapsed;
         }
 
         private ConnectionStatus _status = ConnectionStatus.Disconnected;
@@ -88,12 +119,10 @@ namespace Amazon.AwsToolkit.CodeWhisperer.Credentials
                         throw new InvalidOperationException(msg);
                     }
 
-                    var credentialsProtocol = _codeWhispererLspClient.CreateToolkitLspCredentials();
-                    await credentialsProtocol.UpdateTokenAsync(new BearerToken() { Token = awsToken.Token });
-
-                    _signedInCredentialIdentifier = credentialIdentifier;
-                    Status = ConnectionStatus.Connected;
-                    toolkitContext.ToolkitHost.OutputToHostConsole($"Signed in to CodeWhisperer with {displayName}.");
+                    if (await UseAwsTokenAsync(awsToken, credentialIdentifier, region))
+                    {
+                        toolkitContext.ToolkitHost.OutputToHostConsole($"Signed in to CodeWhisperer with {displayName}.");
+                    }
                 }
                 catch (Exception ex)
                 {
@@ -125,24 +154,20 @@ namespace Amazon.AwsToolkit.CodeWhisperer.Credentials
         /// <summary>
         /// Signs the user out of CodeWhisperer
         /// </summary>
-        public Task SignOutAsync()
+        public async Task SignOutAsync()
         {
-            var displayName = _signedInCredentialIdentifier?.DisplayName ?? "<unknown>";
+            var credentialId = _signedInCredentialIdentifier;
+            var displayName = credentialId?.DisplayName ?? "<unknown>";
 
             try
             {
                 var toolkitContext = _toolkitContextProvider.GetToolkitContext();
 
-                var lspCreds = _codeWhispererLspClient.CreateToolkitLspCredentials();
-                lspCreds.DeleteToken();
-
-                _toolkitContextProvider.GetToolkitContext().CredentialManager.Invalidate(_signedInCredentialIdentifier);
-
-                _signedInCredentialIdentifier = null;
-                Status = ConnectionStatus.Disconnected;
-                toolkitContext.ToolkitHost.OutputToHostConsole($"Sign out of CodeWhisperer with {displayName}.");
-
-                return Task.CompletedTask;
+                if (await ClearAwsTokenAsync())
+                {
+                    _toolkitContextProvider.GetToolkitContext().CredentialManager.Invalidate(credentialId);
+                    toolkitContext.ToolkitHost.OutputToHostConsole($"Signed out of CodeWhisperer with {displayName}.");
+                }
             }
             catch (Exception ex)
             {
@@ -165,6 +190,176 @@ namespace Amazon.AwsToolkit.CodeWhisperer.Credentials
             var toolkitContext = _toolkitContextProvider.GetToolkitContext();
             toolkitContext.ToolkitHost.OutputToHostConsole(message);
             toolkitContext.ToolkitHost.ShowError(message, ex?.Message ?? message);
+        }
+
+        private Task<bool> ClearAwsTokenAsync()
+        {
+            return UseAwsTokenAsync(null, null, null);
+        }
+
+        /// <summary>
+        /// Updates the connection state (and adjusts the refresh timer) based on the given token.
+        /// Signed in and refreshed tokens should be passed to this method.
+        /// To "clear" a token (sign out), pass null for all parameters, or use <see cref="ClearAwsTokenAsync"/>.
+        /// </summary>
+        private async Task<bool> UseAwsTokenAsync(
+            AWSToken token,
+            ICredentialIdentifier credentialIdentifier,
+            ToolkitRegion credentialsSsoRegion)
+        {
+            var credentialsProtocol = _codeWhispererLspClient.CreateToolkitLspCredentials();
+
+            _tokenRefreshTimer.Stop();
+
+            if (token == null)
+            {
+                // Indicates "sign out"
+                credentialsProtocol.DeleteToken();
+
+                _signedInCredentialIdentifier = null;
+                _signedInCredentialsSsoRegion = null;
+                _tokenExpiresAt = null;
+                Status = ConnectionStatus.Disconnected;
+            }
+            else
+            {
+                // Indicates "sign in" or "token refresh"
+
+                // TODO : Soon UpdateTokenAsync will return a response. Return true or false based on the response.
+                await credentialsProtocol.UpdateTokenAsync(new BearerToken() { Token = token.Token });
+
+                _signedInCredentialIdentifier = credentialIdentifier;
+                _signedInCredentialsSsoRegion = credentialsSsoRegion;
+                _tokenExpiresAt = token.ExpiresAt;
+                Status = ConnectionStatus.Connected;
+
+                // Try to refresh this token at some point in the future.
+                if (token.ExpiresAt.HasValue)
+                {
+                    ResetTokenRefreshTimer(token.ExpiresAt.Value);
+                }
+            }
+
+            return true;
+        }
+
+        private static class RefreshOffsets
+        {
+            public const int MinutesBeforeExpiration = 5;
+            public const int MinutesFromNowDuringRefreshWindow = 1;
+            public const int SecondsAfterExpiration = 1;
+            public const int SecondsFromNowAfterExpiration = 3;
+        }
+
+        /// <summary>
+        /// Sets up the system to attempt a refresh at some time in the future
+        /// based on the given token expiration time.
+        /// </summary>
+        private void ResetTokenRefreshTimer(DateTime tokenExpiresAt)
+        {
+            tokenExpiresAt = tokenExpiresAt.ToUniversalTime();
+            var now = DateTime.UtcNow;
+
+            // The soonest we would try to refresh in relation to the token expiration
+            // is five minutes before token expiration. If that has already passed, we
+            // will try in one minute (this allows a refresh attempt to "cool off").
+            var earliestRefreshAttempt = tokenExpiresAt.AddMinutes(-RefreshOffsets.MinutesBeforeExpiration);
+            if (earliestRefreshAttempt < now)
+            {
+                earliestRefreshAttempt = now.AddMinutes(RefreshOffsets.MinutesFromNowDuringRefreshWindow);
+            }
+
+            // The latest we would try to refresh in relation to the token expiration
+            // is one second after token expiration. If that has already passed, we
+            // will try immediately (this will force us into an expired state).
+            var latestRefreshAttempt = tokenExpiresAt.AddSeconds(RefreshOffsets.SecondsAfterExpiration);
+            if (latestRefreshAttempt < now)
+            {
+                latestRefreshAttempt = now.AddSeconds(RefreshOffsets.SecondsFromNowAfterExpiration);
+            }
+
+            var nextInvokeCandidates = new DateTime[]
+            {
+                earliestRefreshAttempt,
+                latestRefreshAttempt,
+            };
+
+            // set timer, based on nextInvokeAt
+            var nextInvokeAt = nextInvokeCandidates
+                .Where(x => x > DateTime.UtcNow)
+                .Min();
+
+            var interval = (nextInvokeAt - DateTime.UtcNow).TotalMilliseconds;
+
+            _tokenRefreshTimer.Interval = interval;
+            _tokenRefreshTimer.Start();
+        }
+
+        private void OnTokenRefreshTimerElapsed(object sender, ToolkitTimerElapsedEventArgs e)
+        {
+            _taskFactoryProvider.JoinableTaskFactory.Run(async () => await OnTokenRefreshTimerElapsedAsync());
+        }
+
+        /// <summary>
+        /// Attempts to refresh the token.
+        /// If not successful, it will either try again shortly, or expire the connection state.
+        /// </summary>
+        private async Task OnTokenRefreshTimerElapsedAsync()
+        {
+            if (!TrySilentRefresh(out var refreshToken))
+            {
+                // Unable to refresh the token (or retrieve the current one)
+                // Move the connection state to Expired, then disconnect.
+                Status = ConnectionStatus.Expired; // event handlers react to this
+                await ClearAwsTokenAsync();
+
+                return;
+            }
+
+            // We either successfully refreshed the token, or AWS SDK gave us the same token back.
+            if (refreshToken.ExpiresAt > _tokenExpiresAt)
+            {
+                // Token Refresh was successful
+                await UseAwsTokenAsync(refreshToken, _signedInCredentialIdentifier, _signedInCredentialsSsoRegion);
+            }
+            else
+            {
+                // AWS SDK gave us the same token. Try again shortly.
+                if (refreshToken.ExpiresAt.HasValue)
+                {
+                    ResetTokenRefreshTimer(refreshToken.ExpiresAt.Value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Attempts to refresh the SSO token for the current signed-in credentialId without
+        /// prompting the user for an SSO login flow.
+        /// </summary>
+        private bool TrySilentRefresh(out AWSToken refreshToken)
+        {
+            refreshToken = null;
+
+            var toolkitContext = _toolkitContextProvider.GetToolkitContext();
+
+            // Try to refresh the token in a way that fails if the user would need to be prompted for a SSO Login
+            if (!_signedInCredentialIdentifier.HasValidToken(SonoCredentialProviderFactory.CodeWhispererSsoSession, toolkitContext.ToolkitHost))
+            {
+                return false;
+            }
+
+            // The token has been refreshed. We can now obtain it through the credentials engine
+            // with confidence that the user will not get prompted to perform an SSO login.
+            var toolkitCredentials = toolkitContext.CredentialManager.GetToolkitCredentials(
+                _signedInCredentialIdentifier, _signedInCredentialsSsoRegion);
+
+            return toolkitCredentials.GetTokenProvider().TryResolveToken(out refreshToken);
+        }
+
+        public void Dispose()
+        {
+            _tokenRefreshTimer.Elapsed -= OnTokenRefreshTimerElapsed;
+            _tokenRefreshTimer?.Dispose();
         }
     }
 }
