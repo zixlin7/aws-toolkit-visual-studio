3aa23cb9611f0975af949230ec4c6fee2d216b10
Ralph Flora <floralph@amazon.com>
2023-10-31 10:54:34 -0700

    Update Getting Started view/viewmodel to go to existing workflow for AWS Explorer (#1637)

--- vspackages/AwsToolkitPackage.Shared/GettingStarted/Services/IGettingStartedCompleted.cs
+++ vspackages/AwsToolkitPackage.Shared/GettingStarted/Services/IGettingStartedCompleted.cs
@@ -1,11 +1,23 @@
 ﻿namespace Amazon.AWSToolkit.VisualStudio.GettingStarted.Services
 {
+    /// <summary>
+    /// Provides access to update final Getting Started screen properties when adding new profiles.
+    /// </summary>
     public interface IGettingStartedCompleted
     {
+        /// <summary>
+        /// Displays invisible/red/green status box when null/false/true.
+        /// </summary>
         bool? Status { get; set; }
 
+        /// <summary>
+        /// Credential type name of successfully added profile.
+        /// </summary>
         string CredentialTypeName { get; set; }
 
+        /// <summary>
+        /// Credential name of successfully added profile.
+        /// </summary>
         string CredentialName { get; set; }
     }
 }
