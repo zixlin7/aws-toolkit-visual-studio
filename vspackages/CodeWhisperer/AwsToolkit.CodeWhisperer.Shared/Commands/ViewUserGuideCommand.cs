0b4bd20d537a4489d723e15fc270d7216a8b7aef
Shruti Sinha <44882001+shruti0085@users.noreply.github.com>
2023-11-06 17:02:09 -0800

    Wire up View User Guide CW command (#1654)

--- vspackages/CodeWhisperer/AwsToolkit.CodeWhisperer.Shared/Commands/ViewUserGuideCommand.cs
+++ vspackages/CodeWhisperer/AwsToolkit.CodeWhisperer.Shared/Commands/ViewUserGuideCommand.cs
@@ -1,11 +1,18 @@
 ﻿using System.Threading.Tasks;
+using System.Windows.Input;
 
+using Amazon.AWSToolkit.Commands;
+using Amazon.AwsToolkit.Telemetry.Events.Core;
+using Amazon.AwsToolkit.Telemetry.Events.Generated;
 using Amazon.AWSToolkit.Context;
 
 namespace Amazon.AwsToolkit.CodeWhisperer.Commands
 {
     public class ViewUserGuideCommand : BaseCommand
     {
+        public const string UserGuideCwUrl =
+            "https://docs.aws.amazon.com/codewhisperer/latest/userguide/getting-started-with-toolkits.html";
+
         public ViewUserGuideCommand(IToolkitContextProvider toolkitContextProvider)
             : base(toolkitContextProvider)
         {
@@ -13,8 +20,25 @@ namespace Amazon.AwsToolkit.CodeWhisperer.Commands
 
         protected override Task ExecuteCoreAsync(object parameter)
         {
-            // TODO : Display CodeWhisperer User Guide
+            var command = CreateUserGuideCommand();
+            command.Execute(null);
             return Task.CompletedTask;
+        }
+
+        private ICommand CreateUserGuideCommand()
+        {
+            var toolkitContext = _toolkitContextProvider.GetToolkitContext();
+
+            void Record(ITelemetryLogger telemetryLogger)
+            {
+                telemetryLogger.RecordAwsHelp(new AwsHelp()
+                {
+                    AwsAccount = MetadataValue.NotApplicable,
+                    AwsRegion = MetadataValue.NotApplicable,
+                });
+            }
+
+            return OpenUrlCommandFactory.Create(toolkitContext, UserGuideCwUrl, Record);
         }
     }
 }
