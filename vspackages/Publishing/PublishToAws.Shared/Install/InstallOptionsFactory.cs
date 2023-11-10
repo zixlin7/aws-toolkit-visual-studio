ecf0c37b9c4d6a9f47b29e9cfee02a0903b11836
Shruti Sinha <44882001+shruti0085@users.noreply.github.com>
2023-11-10 09:50:39 -0800

    Update deploy tool to 1.16.7 to support .NET8 (#1668)
    This change updates the referenced deploy tool to 1.16.7. The biggest change with this update is the support for .NET 8. https://github.com/aws/aws-dotnet-deploy/releases/tag/1.16.7

--- vspackages/Publishing/PublishToAws.Shared/Install/InstallOptionsFactory.cs
+++ vspackages/Publishing/PublishToAws.Shared/Install/InstallOptionsFactory.cs
@@ -5,7 +5,7 @@ namespace Amazon.AWSToolkit.Publish.Install
 {
     public class InstallOptionsFactory
     {
-        private const string VersionRange = "1.15.7";
+        private const string VersionRange = "1.16.7";
 
         public static InstallOptions Create(IToolkitHostInfo toolkitHostInfo)
         {
