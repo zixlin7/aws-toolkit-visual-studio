zixlin7/aws-toolkit-visual-studio

commit 3f55769ae3ab842fc7893991e914952925077037

Support new device flow for SSO/Builder ID (#1769)
This change updates the toolkit to support the new device flow for SSO/Builder ID login that asks users to confirm the user code in the browser instead of copying the code over to their clipboard for use in the login page in browser.
This change adds the following capabilities:

* When the browser login page is opened, the toolkit login dialog now remains open showing a cancellable progress prompt that waits for the browser authentication to complete. Metrics also record error data when a failure occurs

* Users may cancel this prompt but note there is no way to inject cancellation tokens once browser flow has been triggered by the underlying SDK. They may still choose to complete this flow in the browser while having closed/cancelled the dialog in the toolkit. In this case, if a valid token is found on disk the next time toolkit attempts to authenticate, it will pick up the new valid token and connect to AWS

* The login process has also been split into two parts to support the above:
     * It first attempts to load the token without initiating a browser login callback
     * If the above fails, it shows the login dialog to the user
* Cancellation/failures/success states are appropriately displayed to the user with a message in the output window

toolkitcore/AWSToolkit/CommonUI/Dialogs/ISsoLoginDialogFactory.cs
@@ -1,0 +1,16 @@
+﻿using Amazon.Runtime;
+using Amazon.Runtime.Credentials.Internal;
+
+namespace Amazon.AWSToolkit.CommonUI.Dialogs
+{
+    /// <summary>
+    /// Factory responsible for create SSO login dialogs (for IAM Credentials and bearer token auths)
+    /// </summary>
+    public interface ISsoLoginDialogFactory
+    {
+        ISsoLoginDialog CreateSsoIdcLoginDialog(SSOAWSCredentials ssoCredentials);
+
+        ISsoLoginDialog CreateSsoBuilderIdLoginDialog(ISSOTokenManager ssoTokenManager,
+            SSOTokenManagerGetTokenOptions tokenOptions);
+    }
+}

toolkitcore/AWSToolkit/Credentials/Core/AwsSsoCredentials.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 
 using Amazon.AWSToolkit.CommonUI.Dialogs;
+using Amazon.AWSToolkit.CommonUI.Notifications;
 using Amazon.AWSToolkit.Credentials.Sono;
-using Amazon.AWSToolkit.Exceptions;
 using Amazon.AWSToolkit.Shared;
 using Amazon.Runtime;
 using Amazon.Runtime.CredentialManagement;
-
-using log4net;
 
 namespace Amazon.AWSToolkit.Credentials.Core
 {
@@ -16,79 +14,87 @@ namespace Amazon.AWSToolkit.Credentials.Core
     /// </summary>
     public class AwsSsoCredentials : AWSCredentials
     {
-        private static readonly ILog _logger = LogManager.GetLogger(typeof(AwsSsoCredentials));
-
         private readonly IAWSToolkitShellProvider _toolkitShell;
         private readonly CredentialProfile _profile;
         private readonly SSOAWSCredentials _ssoCredentials;
+        private readonly SSOAWSCredentials _silentSsoCredentials;
 
         public AwsSsoCredentials(CredentialProfile profile, IAWSToolkitShellProvider toolkitShell)
         {
-            var ssoCredentials = new SSOAWSCredentials(
-                profile.Options.SsoAccountId,
-                profile.Options.SsoRegion,
-                profile.Options.SsoRoleName,
-                profile.Options.SsoStartUrl,
-                new SSOAWSCredentialsOptions()
-                {
-                    ClientName = SonoProperties.ClientName,
-                    // Must match SSOTokenProvider session or will prompt to login through browser again
-                    SessionName = profile.Options.SsoSession,
-                    SsoVerificationCallback = StartSsoLogin
-                });
-
             _profile = profile;
-            _ssoCredentials = ssoCredentials;
             _toolkitShell = toolkitShell;
+
+            _silentSsoCredentials = CreateSsoCredentials();
+            _ssoCredentials = CreateSsoCredentials();
         }
 
         public override ImmutableCredentials GetCredentials()
         {
             try
             {
-                return _ssoCredentials.GetCredentials();
-            }
-            catch (Exception e)
-            {
-                _toolkitShell.OutputToHostConsole($"Login failed for AWS IAM Identity Center (SSO) based profile {_profile.Name}: {e.Message}", false);
-                throw;
-            }
-        }
-
-        private void StartSsoLogin(SsoVerificationArguments ssoVerification)
-        {
-            try
-            {
-                // Prompt the user to start the SSO Login flow
+                // attempt to get credentials without a user login
+                if (TryGetSilentCredentials(out var credentials))
+                {
+                    return credentials;
+                }
+                // get credentials by prompting for a user login
                 _toolkitShell.ExecuteOnUIThread(() =>
                 {
-                    var dialog = CreateDialog(ssoVerification);
-                    if (!dialog.Show())
+                    using (var dialog = CreateLoginDialog())
                     {
-                        // Throw an exception to break out of the SDK  AWSCredentials.GetCredentials call
-                        throw new UserCanceledException("User declined to start AWS IAM Identity Center (SSO) Login Flow");
+                        var result = dialog.DoLoginFlow();
+                        result.ThrowIfUnsuccessful();
+                        credentials = dialog.Credentials;
                     }
                 });
-
-                _toolkitShell.OutputToHostConsole($"AWS IAM Identity Center (SSO) Login flow started for Credentials: {_profile.Name}", false);
-                _logger.Debug($"AWS IAM Identity Center (SSO) Login flow started for Credentials: {_profile.Name}");
-
+                return credentials;
             }
             catch (Exception e)
             {
-                _logger.Error($"Error starting AWS IAM Identity Center (SSO) Login flow for {_profile.Name}", e);
+                _toolkitShell.OutputToHostConsole(
+                    $"Login failed for AWS IAM Identity Center (SSO) based profile {_profile.Name}: {e.Message}",
+                    false);
                 throw;
             }
         }
 
-        private ISsoLoginDialog CreateDialog(SsoVerificationArguments ssoVerification)
+        private SSOAWSCredentials CreateSsoCredentials()
         {
-            var dialog = _toolkitShell.GetDialogFactory().CreateSsoLoginDialog();
-            dialog.IsBuilderId = false;
-            dialog.UserCode = ssoVerification.UserCode;
-            dialog.LoginUri = ssoVerification.VerificationUri;
-            dialog.CredentialName = _profile.Name;
-            return dialog;
+            return new SSOAWSCredentials(
+                _profile.Options.SsoAccountId,
+                _profile.Options.SsoRegion,
+                _profile.Options.SsoRoleName,
+                _profile.Options.SsoStartUrl,
+                new SSOAWSCredentialsOptions()
+                {
+                    ClientName = SonoProperties.ClientName,
+                    // Must match SSOTokenProvider session or will prompt to login through browser again
+                    SessionName = _profile.Options.SsoSession,
+                    SsoVerificationCallback = obj => throw new Exception("AWS IAM IDC profile requires a login flow")
+                });
+        }
+
+        /// <summary>
+        /// Attempts to retrieve/resolve credentials without performing a user login
+        /// </summary>
+        private bool TryGetSilentCredentials(out ImmutableCredentials credentials)
+        {
+            credentials = null;
+            try
+            {
+                credentials = _silentSsoCredentials.GetCredentials();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private ISsoLoginDialog CreateLoginDialog()
+        {
+            var ssoDialogFactory = _toolkitShell.GetDialogFactory().CreateSsoLoginDialogFactory(_profile.Name);
+            return ssoDialogFactory.CreateSsoIdcLoginDialog(_ssoCredentials);
         }
     }
 }

commit 74d3a8aa2ff47cad558031adc96fdfbc1d0f91d3

Fixes bug where login dialog incorrectly displayed IDC profiles as BuilderId in UI (#1780)
This change fixes a bug where when authenticating to CWSPR with IDC profiles(from both brain icon and getting started), the display text in the login dialog incorrectly showed it as a Login with Builder Id instead of Login with IAM IDC. This was caused due to a regression introduced in PR #1769 which revamped how these params were passed and used during dialog creation.
The builderId boolean flag was not been correctly passed and set on the dialog.

This change also renames the login view models to SsoTokenProviderLoginViewModel and SsoCredentialsProviderLoginViewModel to better indicate the scope they are handling

toolkitcore/AWSToolkit/CommonUI/Dialogs/ISsoLoginDialogFactory.cs
@@ -8,9 +8,9 @@ namespace Amazon.AWSToolkit.CommonUI.Dialogs
     /// </summary>
     public interface ISsoLoginDialogFactory
     {
-        ISsoLoginDialog CreateSsoIdcLoginDialog(SSOAWSCredentials ssoCredentials);
+        ISsoLoginDialog CreateSsoCredentialsProviderLoginDialog(SSOAWSCredentials ssoCredentials);
 
-        ISsoLoginDialog CreateSsoBuilderIdLoginDialog(ISSOTokenManager ssoTokenManager,
-            SSOTokenManagerGetTokenOptions tokenOptions);
+        ISsoLoginDialog CreateSsoTokenProviderLoginDialog(ISSOTokenManager ssoTokenManager,
+            SSOTokenManagerGetTokenOptions tokenOptions, bool isBuilderId);
     }
 }

toolkitcore/AWSToolkit/Credentials/Core/AwsSsoCredentials.cs
@@ -94,7 +94,7 @@ namespace Amazon.AWSToolkit.Credentials.Core
         private ISsoLoginDialog CreateLoginDialog()
         {
             var ssoDialogFactory = _toolkitShell.GetDialogFactory().CreateSsoLoginDialogFactory(_profile.Name);
-            return ssoDialogFactory.CreateSsoIdcLoginDialog(_ssoCredentials);
+            return ssoDialogFactory.CreateSsoCredentialsProviderLoginDialog(_ssoCredentials);
         }
     }
 }

toolkitcore/AWSToolkit/Credentials/Core/ToolkitSsoTokenManager.cs
@@ -28,6 +28,9 @@ namespace Amazon.AWSToolkit.Credentials.Core
             _toolkitShell = toolkitShell;
         }
 
+        public string TokenProviderDisplayName =>
+            _toolkitTokenManagerOptions.IsBuilderId ? "AWS Builder ID" : "AWS IAM Identity Center (SSO)";
+
         public SsoToken GetToken(SSOTokenManagerGetTokenOptions options)
         {
             SetupOptions(options);
@@ -78,7 +81,7 @@ namespace Amazon.AWSToolkit.Credentials.Core
             catch (Exception ex)
             {
                 _toolkitShell.OutputToHostConsole(
-                    $"Login failed for AWS Builder ID {_toolkitTokenManagerOptions.CredentialName}: {ex.Message}");
+                    $"Log in failed for {TokenProviderDisplayName} based Credentials {_toolkitTokenManagerOptions.CredentialName}: {ex.Message}");
                 throw;
             }
         }
@@ -87,7 +90,7 @@ namespace Amazon.AWSToolkit.Credentials.Core
         {
             var ssoDialogFactory = _toolkitShell.GetDialogFactory()
                 .CreateSsoLoginDialogFactory(_toolkitTokenManagerOptions.CredentialName, cancellationToken);
-            return ssoDialogFactory.CreateSsoBuilderIdLoginDialog(_ssoTokenManager, options);
+            return ssoDialogFactory.CreateSsoTokenProviderLoginDialog(_ssoTokenManager, options, _toolkitTokenManagerOptions.IsBuilderId);
         }
 
         /// <summary>
@@ -99,7 +102,7 @@ namespace Amazon.AWSToolkit.Credentials.Core
             {
                 // Setup token manager options without actual sso callback required for user login
                 options.SsoVerificationCallback = args
-                    => throw new Exception("AWS Builder ID requires a login flow");
+                    => throw new Exception($"{TokenProviderDisplayName} requires a login flow");
                 return _ssoTokenManager.GetToken(options);
             }
             catch (Exception)
@@ -118,7 +121,7 @@ namespace Amazon.AWSToolkit.Credentials.Core
             {
                 // Setup token manager options without actual sso callback required for user login
                 options.SsoVerificationCallback = args
-                    => throw new Exception("AWS Builder ID requires a login flow");
+                    => throw new Exception($"{TokenProviderDisplayName} requires a login flow");
                 return await _ssoTokenManager.GetTokenAsync(options, cancellationToken);
             }
             catch (Exception)

toolkitcore/AWSToolkit/Credentials/Core/ToolkitSsoTokenManagerOptions.cs
@@ -10,17 +10,19 @@ namespace Amazon.AWSToolkit.Credentials.Core
         public string ClientName { get; }
         public string ClientType { get; }
         public string CredentialName { get; }
+        public bool IsBuilderId { get; }
         public Action<SsoVerificationArguments> SsoVerificationCallback { get; }
         public IEnumerable<string> Scopes { get; }
 
         public ToolkitSsoTokenManagerOptions(string clientName, string clientType, string credentialName,
-            Action<SsoVerificationArguments> ssoVerificationCallback, IEnumerable<string> scopes)
+            Action<SsoVerificationArguments> ssoVerificationCallback, IEnumerable<string> scopes, bool isBuilderId)
         {
             ClientName = clientName;
             ClientType = clientType;
             CredentialName = credentialName;
             SsoVerificationCallback = ssoVerificationCallback;
             Scopes = scopes;
+            IsBuilderId = isBuilderId;
         }
     }
 }

toolkitcore/AWSToolkit/Credentials/Sono/SonoTokenProviderBuilder.cs
@@ -181,7 +181,9 @@ namespace Amazon.AWSToolkit.Credentials.Sono
 
         private ISSOTokenManager CreateTokenManager()
         {
-            var tokenManagerOptions = new ToolkitSsoTokenManagerOptions(SonoProperties.ClientName, SonoProperties.ClientType, _credentialIdentifier.DisplayName, _ssoCallbackHandler, _scopes);
+            var tokenManagerOptions = new ToolkitSsoTokenManagerOptions(SonoProperties.ClientName,
+                SonoProperties.ClientType, _credentialIdentifier.ProfileName, _ssoCallbackHandler, _scopes,
+                _isBuilderId);
 
             var ssoOidcClient = SSOServiceClientHelpers.BuildSSOIDCClient(_oidcRegion);
 

vspackages/shared/AwsToolkit.VsSdk.Common.Shared/CommonUI/Models/SsoLoginViewModel.cs
@@ -232,19 +232,19 @@ namespace AwsToolkit.VsSdk.Common.CommonUI.Models
         }
     }
 
-    public class SsoBuilderIdLoginViewModel : SsoLoginViewModel
+    public class SsoTokenProviderLoginViewModel : SsoLoginViewModel
     {
         private readonly ISSOTokenManager _ssoTokenManager;
         private readonly SSOTokenManagerGetTokenOptions _tokenManagerOptions;
 
-        public SsoBuilderIdLoginViewModel(ISSOTokenManager ssoTokenManager,
-            SSOTokenManagerGetTokenOptions tokenManagerOptions, ToolkitContext toolkitContext,
+        public SsoTokenProviderLoginViewModel(ISSOTokenManager ssoTokenManager,
+            SSOTokenManagerGetTokenOptions tokenManagerOptions, bool isBuilderId, ToolkitContext toolkitContext,
             JoinableTaskFactory joinableTaskFactory, CancellationToken cancellationToken) : base(toolkitContext,
             joinableTaskFactory, cancellationToken)
         {
             _ssoTokenManager = ssoTokenManager;
             _tokenManagerOptions = tokenManagerOptions;
-            IsBuilderId = true;
+            IsBuilderId = isBuilderId;
         }
 
         protected override async Task ExecuteLoginFlowAsync()
@@ -259,12 +259,12 @@ namespace AwsToolkit.VsSdk.Common.CommonUI.Models
         }
     }
 
-    public class SsoIdcLoginViewModel : SsoLoginViewModel
+    public class SsoCredentialsProviderLoginViewModel : SsoLoginViewModel
     {
         private readonly SSOAWSCredentials _ssoCredentials;
 
 
-        public SsoIdcLoginViewModel(SSOAWSCredentials credentials, ToolkitContext toolkitContext,
+        public SsoCredentialsProviderLoginViewModel(SSOAWSCredentials credentials, ToolkitContext toolkitContext,
             JoinableTaskFactory joinableTaskFactory, CancellationToken cancellationToken) : base(toolkitContext,
             joinableTaskFactory, cancellationToken)
         {

vspackages/shared/AwsToolkit.VsSdk.Common.Shared/CommonUI/SsoLoginDialogFactory.cs
@@ -27,10 +27,10 @@ namespace AwsToolkit.VsSdk.Common.CommonUI
             _joinableTaskFactory = joinableTaskFactory;
         }
 
-        public ISsoLoginDialog CreateSsoIdcLoginDialog(SSOAWSCredentials ssoCredentials)
+        public ISsoLoginDialog CreateSsoCredentialsProviderLoginDialog(SSOAWSCredentials ssoCredentials)
         {
             var viewModel =
-                new SsoIdcLoginViewModel(ssoCredentials, _toolkitContext, _joinableTaskFactory, _cancellationToken)
+                new SsoCredentialsProviderLoginViewModel(ssoCredentials, _toolkitContext, _joinableTaskFactory, _cancellationToken)
                 {
                     CredentialName = _credentialName
                 };
@@ -38,11 +38,11 @@ namespace AwsToolkit.VsSdk.Common.CommonUI
             return new SsoLoginDialog { DataContext = viewModel };
         }
 
-        public ISsoLoginDialog CreateSsoBuilderIdLoginDialog(ISSOTokenManager ssoTokenManager,
-            SSOTokenManagerGetTokenOptions tokenOptions)
+        public ISsoLoginDialog CreateSsoTokenProviderLoginDialog(ISSOTokenManager ssoTokenManager,
+            SSOTokenManagerGetTokenOptions tokenOptions, bool isBuilderId)
         {
-            var viewModel = new SsoBuilderIdLoginViewModel(ssoTokenManager, tokenOptions, _toolkitContext,
-                _joinableTaskFactory, _cancellationToken) { CredentialName = _credentialName };
+            var viewModel = new SsoTokenProviderLoginViewModel(ssoTokenManager, tokenOptions, isBuilderId, _toolkitContext,
+                _joinableTaskFactory, _cancellationToken) { CredentialName = _credentialName};
             return new SsoLoginDialog { DataContext = viewModel };
         }
     }

tests/AWSToolkit.Tests/Credentials/Core/ToolkitSsoTokenManagerTests.cs
@@ -38,7 +38,7 @@ namespace AWSToolkit.Tests.Credentials.Core
             SetupLoginDialog();
 
             _toolkitOptions = new ToolkitSsoTokenManagerOptions("client-name", "client-type", "credential-name", _ssoCallbackStub,
-                new[] { "scope" });
+                new[] { "scope" }, false);
 
             _sut = new ToolkitSsoTokenManager(_ssoTokenManager.Object, _toolkitOptions, _toolkitContextFixture.ToolkitHost.Object);
         }
@@ -58,13 +58,13 @@ namespace AWSToolkit.Tests.Credentials.Core
 
             SetupSsoTokenManager(null);
             _toolkitOptions = new ToolkitSsoTokenManagerOptions("client-name", "client-type", "credential-name", null,
-                new[] { "scope" });
+                new[] { "scope" }, true);
             var sut = new ToolkitSsoTokenManager(_ssoTokenManager.Object, _toolkitOptions,
                 _toolkitContextFixture.ToolkitHost.Object);
 
             sut.GetToken(_options);
 
-            _ssoDialogFactory.Verify(x => x.CreateSsoBuilderIdLoginDialog(It.IsAny<ISSOTokenManager>(), It.IsAny<SSOTokenManagerGetTokenOptions>()), Times.Once);
+            _ssoDialogFactory.Verify(x => x.CreateSsoTokenProviderLoginDialog(It.IsAny<ISSOTokenManager>(), It.IsAny<SSOTokenManagerGetTokenOptions>(), true), Times.Once);
 
         }
 
@@ -84,7 +84,7 @@ namespace AWSToolkit.Tests.Credentials.Core
         {
             SetupSsoTokenManager(null);
             _toolkitOptions = new ToolkitSsoTokenManagerOptions("client-name", "client-type", "credential-name", null,
-                new[] { "scope" });
+                new[] { "scope" }, true);
 
            
             var sut = new ToolkitSsoTokenManager(_ssoTokenManager.Object, _toolkitOptions,
@@ -92,7 +92,7 @@ namespace AWSToolkit.Tests.Credentials.Core
 
             await sut.GetTokenAsync(_options);
 
-            _ssoDialogFactory.Verify(x => x.CreateSsoBuilderIdLoginDialog(It.IsAny<ISSOTokenManager>(), It.IsAny<SSOTokenManagerGetTokenOptions>()), Times.Once);
+            _ssoDialogFactory.Verify(x => x.CreateSsoTokenProviderLoginDialog(It.IsAny<ISSOTokenManager>(), It.IsAny<SSOTokenManagerGetTokenOptions>(), It.IsAny<bool>()), Times.Once);
 
         }
 
@@ -111,8 +111,8 @@ namespace AWSToolkit.Tests.Credentials.Core
             _toolkitContextFixture.SetupExecuteOnUIThread();
 
             _dialog.Setup(x => x.DoLoginFlow()).Returns(new TaskResult() { Status = TaskStatus.Success });
-            _ssoDialogFactory.Setup(x => x.CreateSsoBuilderIdLoginDialog(It.IsAny<ISSOTokenManager>(),
-                It.IsAny<SSOTokenManagerGetTokenOptions>())).Returns(_dialog.Object);
+            _ssoDialogFactory.Setup(x => x.CreateSsoTokenProviderLoginDialog(It.IsAny<ISSOTokenManager>(),
+                It.IsAny<SSOTokenManagerGetTokenOptions>(), It.IsAny<bool>())).Returns(_dialog.Object);
             _toolkitContextFixture.ToolkitHost.Setup(x =>
                     x.GetDialogFactory().CreateSsoLoginDialogFactory(It.IsAny<string>(), It.IsAny<CancellationToken>()))
                 .Returns(_ssoDialogFactory.Object);

tests/AwsToolkit.Vs.Tests.Shared/VsSdk.Common/CommonUI/SsoLoginViewModelTests.cs
@@ -18,7 +18,7 @@ using TaskStatus = Amazon.AWSToolkit.CommonUI.Notifications.TaskStatus;
 
 namespace AwsToolkit.Vs.Tests.VsSdk.Common.CommonUI
 {
-    public class TestSsoLoginViewModel : SsoBuilderIdLoginViewModel
+    public class TestSsoTokenProviderLoginViewModel : SsoTokenProviderLoginViewModel
     {
         public async Task ExposeBeginLoginFlowAsync()
         {
@@ -27,9 +27,9 @@ namespace AwsToolkit.Vs.Tests.VsSdk.Common.CommonUI
 
         public CancellationToken CancellationToken => _linkedCancellationToken;
 
-        public TestSsoLoginViewModel(ISSOTokenManager ssoTokenManager,
+        public TestSsoTokenProviderLoginViewModel(ISSOTokenManager ssoTokenManager,
             SSOTokenManagerGetTokenOptions tokenManagerOptions, ToolkitContext toolkitContext,
-            JoinableTaskFactory joinableTaskFactory, CancellationToken cancellationToken) : base(ssoTokenManager, tokenManagerOptions, toolkitContext,
+            JoinableTaskFactory joinableTaskFactory, CancellationToken cancellationToken) : base(ssoTokenManager, tokenManagerOptions, true, toolkitContext,
             joinableTaskFactory, cancellationToken)
         {
         }
@@ -38,7 +38,7 @@ namespace AwsToolkit.Vs.Tests.VsSdk.Common.CommonUI
     public class SsoLoginViewModelTests : IDisposable
     {
         private readonly ToolkitContextFixture _toolkitContextFixture = new ToolkitContextFixture();
-        private readonly TestSsoLoginViewModel _sut;
+        private readonly TestSsoTokenProviderLoginViewModel _sut;
         private readonly SsoToken _sampleSsoToken = new SsoToken();
         private readonly Mock<ISSOTokenManager> _ssoTokenManager = new Mock<ISSOTokenManager>();
         private readonly SSOTokenManagerGetTokenOptions _options = new SSOTokenManagerGetTokenOptions();
@@ -48,7 +48,7 @@ namespace AwsToolkit.Vs.Tests.VsSdk.Common.CommonUI
 #pragma warning disable VSSDK005 // ThreadHelper.JoinableTaskContext requires VS Services from a running VS instance
             var taskContext = new JoinableTaskContext();
 #pragma warning restore VSSDK005
-            _sut = new TestSsoLoginViewModel(_ssoTokenManager.Object, _options, _toolkitContextFixture.ToolkitContext,
+            _sut = new TestSsoTokenProviderLoginViewModel(_ssoTokenManager.Object, _options, _toolkitContextFixture.ToolkitContext,
                 taskContext.Factory, default);
         }
 