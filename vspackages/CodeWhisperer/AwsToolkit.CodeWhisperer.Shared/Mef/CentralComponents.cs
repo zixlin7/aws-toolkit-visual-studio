579984f32eca5aea61ba9e0a1c8704a1f8b1362c
Christopher Christou <39839589+awschristou@users.noreply.github.com>
2024-02-16 16:09:50 -0800

    Emit CodeWhisperer enabled state on startup and when it changes (#1789)
    We want to understand how many users disable CodeWhisperer. In order to do this, we will measure whenever users enable or disable CodeWhisperer. Because this doesn't tell us if CodeWhisperer has already been previously disabled, we also measure this setting on Toolkit startup.
    
    To support this change, central MEF components that were imported through CodeWhispererMarginProvider have been moved to their own CentralComponents component. This reduces the amount of extraneous imports that go through the CodeWhispererMarginProvider ctor.

---
+++ vspackages/CodeWhisperer/AwsToolkit.CodeWhisperer.Shared/Mef/CentralComponents.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Composition;
+
+using Amazon.AwsToolkit.CodeWhisperer.Credentials;
+using Amazon.AwsToolkit.CodeWhisperer.Settings;
+using Amazon.AwsToolkit.CodeWhisperer.Telemetry;
+
+namespace Amazon.AwsToolkit.CodeWhisperer.Mef
+{
+    /// <summary>
+    /// A MEF component containing central CodeWhisperer componenets that are not automatically activated by
+    /// Visual Studio or the VS SDK.
+    ///
+    /// These are components we want to instantiate shortly after the IDE starts.
+    /// By placing them here, we import this class in one of the components that *is*
+    /// instantiated by Visual Studio (<seealso cref="Margins.CodeWhispererMarginProvider"/>), and this avoids 
+    /// having a list of arbitrary imports in that class.
+    ///
+    /// Components imported here are typically systems that react to other events.
+    /// </summary>
+    [Export(typeof(CentralComponents))]
+    internal class CentralComponents
+    {
+        [ImportingConstructor]
+        public CentralComponents(
+            IExpirationNotificationManager expirationNotificationManager,
+            ICodeWhispererSettingsPublisher codeWhispererSettingsPublisher,
+            ICodeWhispererTelemetryEventPublisher codeWhispererTelemetryEventPublisher,
+            ICodeWhispererEnabledStateEmitter enabledStateEmitter,
+            EnabledStateManager enabledStateManager)
+        {
+        }
+    }
+}
---
+++ vspackages/CodeWhisperer/AwsToolkit.CodeWhisperer.Shared/Settings/CodeWhispererEnabledStateEmitter.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+
+using Amazon.AwsToolkit.Telemetry.Events.Core;
+using Amazon.AwsToolkit.Telemetry.Events.Generated;
+using Amazon.AwsToolkit.VsSdk.Common.Tasks;
+using Amazon.AWSToolkit.Context;
+
+using log4net;
+
+namespace Amazon.AwsToolkit.CodeWhisperer.Settings
+{
+    /// <summary>
+    /// MEF Component interface responsible for emitting metrics whenever CodeWhisperer is enabled
+    /// or disabled. It also records the current state on startup.
+    /// </summary>
+    public interface ICodeWhispererEnabledStateEmitter
+    {
+    }
+
+    /// <summary>
+    /// CodeWhisperer MEF component responsible for emitting "is CodeWhisperer enabled?" metrics
+    /// </summary>
+    [Export(typeof(ICodeWhispererEnabledStateEmitter))]
+    internal class CodeWhispererEnabledStateEmitter : ICodeWhispererEnabledStateEmitter, IDisposable
+    {
+        private static class MetricSources
+        {
+            public const string AtStartup = "startup";
+            public const string InSettings = "settings";
+        }
+
+        private static readonly ILog _logger = LogManager.GetLogger(typeof(CodeWhispererEnabledStateEmitter));
+
+        private readonly ICodeWhispererSettingsRepository _settingsRepository;
+        private readonly IToolkitContextProvider _toolkitContextProvider;
+        private bool _isDisposed = false;
+        private bool? _previousEnabledState = null;
+
+        [ImportingConstructor]
+        public CodeWhispererEnabledStateEmitter(ICodeWhispererSettingsRepository settingsRepository,
+            IToolkitContextProvider toolkitContextProvider,
+            ToolkitJoinableTaskFactoryProvider taskFactoryProvider)
+        {
+            _settingsRepository = settingsRepository;
+            _toolkitContextProvider = toolkitContextProvider;
+
+            taskFactoryProvider.JoinableTaskFactory.Run(InitializeAsync);
+        }
+
+        private async Task InitializeAsync()
+        {
+            var settings = await _settingsRepository.GetAsync();
+            _previousEnabledState = settings.IsEnabled;
+
+            // Emit the current value once the Toolkit is initialized
+            _toolkitContextProvider.RegisterOnInitializedCallback(() => RecordEnabledState(MetricSources.AtStartup, settings.IsEnabled));
+
+            // Now that we've loaded settings, emit whenever the enabled state changes
+            _settingsRepository.SettingsSaved += OnSettingsRepositorySaved;
+        }
+
+        private void OnSettingsRepositorySaved(object sender, CodeWhispererSettingsSavedEventArgs e)
+        {
+            if (!_previousEnabledState.HasValue || _previousEnabledState.Value != e.Settings.IsEnabled)
+            {
+                RecordEnabledState(MetricSources.InSettings, e.Settings.IsEnabled);
+            }
+
+            _previousEnabledState = e.Settings.IsEnabled;
+        }
+
+        private void RecordEnabledState(string source, bool isEnabled)
+        {
+            try
+            {
+                _toolkitContextProvider.GetToolkitContext().TelemetryLogger.RecordCodewhispererEnabled(new CodewhispererEnabled()
+                {
+                    Source = source,
+                    Enabled = isEnabled,
+                    AwsAccount = MetadataValue.NotApplicable,
+                    AwsRegion = MetadataValue.NotApplicable,
+                });
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Unable to emit CodeWhisperer Enabled metric", ex);
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    _settingsRepository.SettingsSaved -= OnSettingsRepositorySaved;
+                }
+
+                _isDisposed = true;
+            }
+        }
+    }
+}
